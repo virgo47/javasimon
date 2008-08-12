537fb9e0cf14d4dab9f36bcb44b194027d8fe2a6
jozef.babjak <jozef.babjak@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2008-08-12 19:12:47 +0000

    Javadoc added.
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@26 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- src/org/javasimon/SimonState.java
+++ src/org/javasimon/SimonState.java
@@ -1,13 +1,26 @@
 package org.javasimon;
 
 /**
- * SimonStatus.
+ * Status of monitor.
  *
  * @author <a href="mailto:virgo47@gmail.com">Richard "Virgo" Richter</a>
  * @created Aug 4, 2008
  */
 public enum SimonState {
+
+	/**
+	 * Enabled.
+	 */
 	ENABLED,
+
+	/**
+	 * Disabled.
+	 */
 	DISABLED,
+
+	/**
+	 * Effective state (enabled/disabled) is going to be derived from a parrent monitor.
+	 */
 	INHERIT
+	
 }
