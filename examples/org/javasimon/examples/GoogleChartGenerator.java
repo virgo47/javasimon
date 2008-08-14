83a58a69fcac5ede0b1b6aecc2c8b51088aca19f
Jozef Babjak <jozef.babjak>
2008-08-14 06:40:53 +0000

    Really unnecessary refactoring, but I cannot resist. :-)

--- examples/org/javasimon/examples/GoogleChartGenerator.java
+++ examples/org/javasimon/examples/GoogleChartGenerator.java
@@ -3,6 +3,9 @@ package org.javasimon.examples;
 import org.javasimon.Simon;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.text.DecimalFormat;
 
 /**
@@ -16,21 +19,29 @@ public final class GoogleChartGenerator {
 	private static final String TYPE_BAR = "&cht=bvg&chbh=32,10,60&chco=4d89f9,c6d9fd&chxt=x,x,y";
 	private static final DecimalFormat nf = new DecimalFormat("0.00");
 
-	public static String barChart(DataCollector collector, String title, double divisor, String unit) {
-		StringBuilder sb = new StringBuilder(URL_START).append(TYPE_BAR);
-		sb.append("&chtt=").append(encode(title));
-		StringBuilder x0 = new StringBuilder("&chxl=0:");
-		StringBuilder x1 = new StringBuilder("|1:");
-		StringBuilder data = new StringBuilder("&chd=t:");
+    private static final List<Replacement> REPLACEMENTS = new LinkedList<Replacement>();
+
+    static {
+        REPLACEMENTS.add(new Replacement("\\+", "%2b"));
+        REPLACEMENTS.add(new Replacement(" ", "+"));
+        REPLACEMENTS.add(new Replacement("&", "%26"));
+    }
+
+    public static String barChart(DataCollector collector, String title, double divisor, String unit) {
+		final StringBuilder result = new StringBuilder(URL_START).append(TYPE_BAR);
+		result.append("&chtt=").append(encode(title));
+		final StringBuilder x0 = new StringBuilder("&chxl=0:");
+		final StringBuilder x1 = new StringBuilder("|1:");
+		final StringBuilder data = new StringBuilder("&chd=t:");
 		double max = 0;
 		boolean first = true;
-		for (Simon simon : collector.getSimons()) {
+		for (final Simon simon : collector.getSimons()) {
 			if (first) {
 				first = false;
 			} else {
 				data.append(',');
 			}
-			List<Double> values = collector.valuesFor(simon);
+			final List<Double> values = collector.valuesFor(simon);
 			double lastValue = values.get(values.size() - 1) / divisor;
 			x0.append('|').append(encode(simon.getName()));
 			String formattedValue = nf.format(lastValue);
@@ -46,17 +57,37 @@ public final class GoogleChartGenerator {
 		for (; x < max + division; x += division) {
 			x2.append('|').append(Double.valueOf(x).longValue());
 		}
-		sb.append("&chxr=2,0,").append(Double.valueOf(x - division).longValue());
-		sb.append("&chds=0,").append(Double.valueOf(x - division).longValue());
-		sb.append(x0).append(x1).append(x2).append(data);
-		sb.append("&.png");
-		return sb.toString();
+		result.append("&chxr=2,0,").append(Double.valueOf(x - division).longValue());
+		result.append("&chds=0,").append(Double.valueOf(x - division).longValue());
+		result.append(x0).append(x1).append(x2).append(data);
+		result.append("&.png");
+		return result.toString();
 	}
 
-	private static String encode(String title) {
-		title = title.replaceAll("\\+", "%2b");
-		title = title.replaceAll(" ", "+");
-		title = title.replaceAll("&", "%26");
-		return title;
+	private static String encode(String s) {
+        for (final Replacement replacement : REPLACEMENTS) {
+            s = s.replaceAll(replacement.from(), replacement.to());
+        }
+		return s;
 	}
+
+    private static final class Replacement {
+
+        private final String from;
+
+        private final String to;
+
+        private Replacement(final String from, final String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public String from() {
+            return from;
+        }
+
+        public String to() {
+            return to;
+        }
+    }
 }
