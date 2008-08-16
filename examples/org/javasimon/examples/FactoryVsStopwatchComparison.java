6ddd7c14d631939627260a3ed43ee9a9a5b6571b
Richard 'Virgo' Richter <virgo47@gmail.com>
2008-08-16 21:17:01 +0000

    META-INF/service/java.sql.Driver to the JAR + Factory vs. Stopwatch example now makes sense again.

--- examples/org/javasimon/examples/FactoryVsStopwatchComparison.java
+++ examples/org/javasimon/examples/FactoryVsStopwatchComparison.java
@@ -5,7 +5,10 @@ import org.javasimon.utils.SimonUtils;
 import org.javasimon.Stopwatch;
 
 /**
- * FactoryVsStopwatchComparison.
+ * FactoryVsStopwatchComparison compares obtaining Simon from the factory with start/stop method calls.
+ * It also measures loop containing get+start/stop construction, so you can get the idea of how fast
+ * it is. Generally - it's not necessary to hold the Simon in a class field but if you measure something
+ * critical, you can do that and get some time.
  *
  * @author <a href="mailto:virgo47@gmail.com">Richard "Virgo" Richter</a>
  * @created Aug 6, 2008
@@ -20,31 +23,45 @@ public final class FactoryVsStopwatchComparison {
 
 	public static void main(String[] args) {
 		// warmup
-		factoryTest();
+		Stopwatch tested = SimonFactory.getStopwatch(NAME);
+		getStopwatchTest();
+		startStopTest(tested);
+		getStartStopTest();
 
-		long ns = System.nanoTime();
-		factoryTest();
-		System.out.println("SimonFactory.getStopwatch(): " + SimonUtils.presentNanoTime(System.nanoTime() - ns));
+		Stopwatch stopwatch = SimonFactory.getStopwatch(null).start();
+		getStopwatchTest();
+		System.out.println("getStopwatch: " + SimonUtils.presentNanoTime(stopwatch.stop()));
 
-		Stopwatch stopwatch = SimonFactory.getStopwatch(NAME);
-		stopwatchTest(stopwatch);
+		tested.reset();
+		stopwatch.reset().start();
+		startStopTest(tested);
+		System.out.println("start/stop: " + SimonUtils.presentNanoTime(stopwatch.stop()));
+		System.out.println("Stopwatch: " + tested);
 
-		ns = System.nanoTime();
-		stopwatchTest(stopwatch);
-		System.out.println("Stopwatch start/stop: " + SimonUtils.presentNanoTime(System.nanoTime() - ns));
-		System.out.println("Stopwatch: " + stopwatch);
+		tested.reset();
+		stopwatch.reset().start();
+		getStartStopTest();
+		System.out.println("get+start/stop: " + SimonUtils.presentNanoTime(stopwatch.stop()));
+		System.out.println("Stopwatch: " + tested);
 	}
 
-	private static void stopwatchTest(Stopwatch stopwatch) {
+	private static void getStopwatchTest() {
+		for (int i = 0; i < LOOP; i++) {
+			SimonFactory.getStopwatch(NAME);
+		}
+	}
+
+	private static void startStopTest(Stopwatch stopwatch) {
 		for (int i = 0; i < LOOP; i++) {
 			stopwatch.start();
 			stopwatch.stop();
 		}
 	}
 
-	private static void factoryTest() {
+	private static void getStartStopTest() {
 		for (int i = 0; i < LOOP; i++) {
-			SimonFactory.getStopwatch(NAME);
+			Stopwatch stopwatch = SimonFactory.getStopwatch(NAME).start();
+			stopwatch.stop();
 		}
 	}
 }
