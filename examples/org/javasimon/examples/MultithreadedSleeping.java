038e3c8c032e267c4e4dff279421ab06b52d1b75
Richard 'Virgo' Richter <virgo47@gmail.com>
2008-09-08 23:13:01 +0000

    Little toString fix and one javadoc.

--- examples/org/javasimon/examples/MultithreadedSleeping.java
+++ examples/org/javasimon/examples/MultithreadedSleeping.java
@@ -6,7 +6,15 @@ import org.javasimon.Stopwatch;
 import java.util.concurrent.CountDownLatch;
 
 /**
- * MultithreadSleeping.
+ * MultithreadSleeping shows that total stopwatch time can be easily much bigger
+ * than total run-time of the application. Typical output of this program is:
+ * <pre>
+ * Simon: Simon Stopwatch: [org.javasimon.examples.MultithreadedSleeping INHERIT/stats=NULL] total 100.0 s, counter 100, max 1.00 s, min 999 ms
+ * </pre>
+ * This shows that whenever wait (IO, sleep) is measured you can get times
+ * multiplied by the number of threads. Another thing you can notice is not
+ * perfect precision of sleep because in theory it should not sleep less than
+ * 1 second. Either sleep or nanoTime is not perfect.
  *
  * @author <a href="mailto:virgo47@gmail.com">Richard "Virgo" Richter</a>
  * @created Aug 6, 2008
@@ -15,6 +23,7 @@ public final class MultithreadedSleeping extends Thread {
 	private static final int THREADS = 100;
 	private static final int SLEEP = 1000;
 
+	// name of the Simon will be the same like the name of this class
 	private static final String NAME = SimonManager.generateName(null, false);
 
 	private static final CountDownLatch latch = new CountDownLatch(THREADS);
@@ -23,6 +32,7 @@ public final class MultithreadedSleeping extends Thread {
 		for (int i = 0; i < THREADS; i++) {
 			new MultithreadedSleeping().start();
 		}
+		// here we wait for all threads to end
 		latch.await();
 		System.out.println("Simon: " + SimonManager.getStopwatch(NAME));
 	}
@@ -36,6 +46,7 @@ public final class MultithreadedSleeping extends Thread {
 			e.printStackTrace();
 		}
 		stopwatch.stop();
+		// signal to latch that the thread is finished
 		latch.countDown();
 	}
 }
