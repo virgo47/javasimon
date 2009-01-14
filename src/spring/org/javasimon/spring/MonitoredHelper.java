75af0bdfa50b58530f830667fdd1d6bb2a185d0e
virgo47 <virgo47@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2009-01-14 12:38:59 +0000

    Added Erik's Spring support via Monitored annotation.
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@147 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

---
+++ src/spring/org/javasimon/spring/MonitoredHelper.java
@@ -0,0 +1,45 @@
+package org.javasimon.spring;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.support.AopUtils;
+
+/**
+ * Utility class that can be used along side the {@link Monitored} annotation.
+ *
+ * @author Erik van Oosten
+ */
+abstract class MonitoredHelper {
+	/**
+	 * Determine monitor name for a method invocation.
+	 *
+	 * @param invocation the method invocation (not null)
+	 * @return the monitor name for this invocation
+	 */
+	public static String getMonitorName(MethodInvocation invocation) {
+		String classPart = getClassPart(invocation);
+		String methodPart = getMethodPart(invocation);
+		return classPart + '.' + methodPart;
+	}
+
+	private static String getClassPart(MethodInvocation invocation) {
+		Class targetClass = AopUtils.getTargetClass(invocation.getThis());
+		Monitored classAnnotation = (Monitored) targetClass.getAnnotation(Monitored.class);
+		if (classAnnotation == null || classAnnotation.name() == null || classAnnotation.name().length() == 0) {
+			return targetClass.getName();
+		} else {
+			return classAnnotation.name();
+		}
+	}
+
+	private static String getMethodPart(MethodInvocation invocation) {
+		Monitored methodAnnotation = invocation.getMethod().getAnnotation(Monitored.class);
+		if (methodAnnotation == null || methodAnnotation.name() == null || methodAnnotation.name().length() == 0) {
+			return invocation.getMethod().getName();
+		} else {
+			return methodAnnotation.name();
+		}
+	}
+
+	private MonitoredHelper() {
+	}
+}
---
+++ src/spring/org/javasimon/spring/MonitoringInterceptor.java
@@ -0,0 +1,27 @@
+package org.javasimon.spring;
+
+import java.io.Serializable;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.javasimon.SimonManager;
+import org.javasimon.Split;
+
+/**
+ * Method interceptor that measures the duration of the intercepted call with a Stopwatch.
+ *
+ * @author Erik van Oosten
+ */
+public class MonitoringInterceptor implements MethodInterceptor, Serializable {
+
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		String monitorName = MonitoredHelper.getMonitorName(invocation);
+
+		Split split = SimonManager.getStopwatch(monitorName).start();
+		try {
+			return invocation.proceed();
+		} finally {
+			split.stop();
+		}
+	}
+}
