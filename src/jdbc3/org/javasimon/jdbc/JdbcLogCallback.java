d80e0cb9046ce5a1681e926a107c15f7763a564c
radovan.sninsky@gmail.com <radovan.sninsky@gmail.com@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2009-02-18 22:20:31 +0000

    Added more options to jdbc logging feature, testing and proper javadocs are still on todo list
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@179 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- src/jdbc3/org/javasimon/jdbc/JdbcLogCallback.java
+++ src/jdbc3/org/javasimon/jdbc/JdbcLogCallback.java
@@ -41,111 +41,139 @@ import java.io.IOException;
  SimonManager.installCallback(filter);
  * </pre>
  * @author Radovan Sninsky
- * @version $Revision: $ $Date: $
+ * @version $Revision$ $Date$
  * @created 30.1.2009 15:26:00
- * @since 1.0
+ * @since 2
  */
-public class JdbcLogCallback extends CallbackSkeleton {
+final class JdbcLogCallback extends CallbackSkeleton {
 
-	private final Logger logger = Logger.getLogger("simon_jdbc_logger");
+	private final class HumanFormatter extends SimonFormatter {
 
-	/**
-	 * Trieda MyFormatter.
-	 *
-	 * @author Radovan Sninsky
-	 * @version $ Revision $ $ Date $
-	 * @created 30.05.2003 14:56:29
-	 * @since 2
-	 */
-	private class HumanFormatter extends Formatter {
+		private static final String ID = "human";
 
-		private Date dat = new Date();
-		private SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss.SSS");
-		private String lineSeparator = System.getProperty("line.separator");
+		private final Date dat = new Date();
+		private final SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss.SSS");
+		private final String lineSeparator = System.getProperty("line.separator");
 
-		/**
-		 * Naformatovanie logovacej spravy.
-		 *
-		 * @param record logovacia sprava
-		 * @return naformatovay retazec
-		 */
-		public synchronized String format(LogRecord record) {
+		protected String formatRecord(LogRecord record, LogParams params) {
 			dat.setTime(record.getMillis());
 			StringBuilder sb = new StringBuilder();
 			sb.append(dateTimeFormat.format(dat)).append(' ');
 			sb.append(record.getThreadID()).append(' ');
 
-			String ln = SimonUtils.localName(fullName(record));
+			String ln = params.getLocalName();
 			sb.append('[').append(ln.equals("conn") || ln.equals("stmt") ? ln : "sql ")
-				.append(isStart(record) ? "->" : "-<").append("]").append(' ');
-			sb.append(fullName(record)).append(' ');
-			sb.append(split(record)).append(' ');
-			sb.append(note(record)).append(lineSeparator);
+				.append(isStart(params) ? "->" : "-<").append("]").append(' ');
+			sb.append(params.getFullName()).append(' ');
+			sb.append(SimonUtils.presentNanoTime(params.getSplit())).append(' ');
+			sb.append(note(params)).append(lineSeparator);
 			return sb.toString();
-		}
-
-		protected String fullName(LogRecord r) {
-			return (String)r.getParameters()[0];
-		}
-
-		protected boolean isStart(LogRecord r) {
-			return r.getParameters()[1].equals("start");
-		}
-
-		protected String split(LogRecord r) {
-			Object o = r.getParameters()[2];
-			return o != null ? SimonUtils.presentNanoTime((Long)o) : "";
-		}
-
-		protected String note(LogRecord r) {
-			Object o = r.getParameters()[3];
-			return o != null ? "{"+o+"}" : "";
 		}
 	}
 
-	private class CsvFormatter extends HumanFormatter {
+	private final class CsvFormatter extends SimonFormatter {
 
-		private String lineSeparator = System.getProperty("line.separator");
+		private static final String ID = "human";
 
-		public synchronized String format(LogRecord record) {
+		private final String lineSeparator = System.getProperty("line.separator");
+
+		protected String formatRecord(LogRecord record, LogParams params) {
 			StringBuilder sb = new StringBuilder();
 			sb.append(record.getMillis()).append('|');
 			sb.append(record.getThreadID()).append('|');
 
-			String ln = SimonUtils.localName(fullName(record));
+			String ln = params.getLocalName();
 			sb.append(ln.equals("conn") || ln.equals("stmt") ? ln : "sql")
-				.append(isStart(record) ? "|>" : "|<").append('|');
-			sb.append(fullName(record)).append('|');
-			sb.append(split(record)).append('|');
-			sb.append(note(record)).append(lineSeparator);
+				.append(isStart(params) ? "|>" : "|<").append('|');
+			sb.append(params.getFullName()).append('|');
+			sb.append(params.getSplit()).append('|');
+			sb.append(note(params)).append(lineSeparator);
 			return sb.toString();
-		}
-
-		protected String split(LogRecord r) {
-			Object o = r.getParameters()[2];
-			return o != null ? o.toString() : "";
-		}
-
-		protected String note(LogRecord r) {
-			Object o = r.getParameters()[3];
-			return o != null ? o.toString() : "";
 		}
 	}
 
-	public JdbcLogCallback(String filename) {
-		logger.setUseParentHandlers(false);
-//		logger.addHandler(new StreamHandler(System.out, new HumanFormatter()));
-//		logger.addHandler(new StreamHandler(System.out, new CsvFormatter()));
-		try {
-			FileHandler fh = new FileHandler(filename);
-			fh.setFormatter(new CsvFormatter());
-			logger.addHandler(fh);
-		} catch (IOException e) {
-			// todo do something
+	private String logFilename;
+	private String loggerName;
+	private boolean logToConsole;
+	private String logFormat;
+
+	private Logger logger;
+
+	private boolean initialized = false;
+
+	public JdbcLogCallback() {
+	}
+
+	public void setLogFilename(String logFilename) {
+		this.logFilename = logFilename;
+	}
+
+	public void setLoggerName(String loggerName) {
+		this.loggerName = loggerName;
+	}
+
+	public void setLogToConsole(boolean logToConsole) {
+		this.logToConsole = logToConsole;
+	}
+
+	public void setLogFormat(String logFormat) {
+		this.logFormat = logFormat;
+	}
+
+	private void initialize() {
+		logger = Logger.getLogger(loggerName != null && loggerName.length() > 0 ? loggerName : "simon_jdbc_logger");
+
+		if (logFilename != null && logFilename.length() > 0) {
+			logger.setUseParentHandlers(false);
+			try {
+				FileHandler fh = new FileHandler(logFilename);
+				SimonFormatter f = formatter();
+				if (f != null) {
+					fh.setFormatter(f);
+				}
+				logger.addHandler(fh);
+			} catch (IOException e) {
+				// todo do something
+			}
 		}
+
+		if (logToConsole) {
+			ConsoleHandler ch = new ConsoleHandler();
+			SimonFormatter f = formatter();
+			if (f != null) {
+				ch.setFormatter(f);
+			}
+			logger.addHandler(ch);
+		}
+		
+		initialized = true;
+	}
+
+	private SimonFormatter formatter() {
+		if (logFormat != null) {
+			if (logFormat.equalsIgnoreCase(HumanFormatter.ID)) {
+				return new HumanFormatter();
+			} else if (logFormat.equalsIgnoreCase(CsvFormatter.ID)) {
+				return new CsvFormatter();
+			} else {
+				try {
+					Object o = Class.forName(logFormat).newInstance();
+					if (o instanceof SimonFormatter) {
+						return (SimonFormatter)o;
+					}
+				} catch (Exception e) {
+					return null;
+				}
+			}
+		}
+		return null;
 	}
 
 	public void stopwatchStart(Split split) {
+		if (!initialized) {
+			initialize();
+		}
+
 		String fullName = split.getStopwatch().getName();
 		String localName = SimonUtils.localName(fullName);
 		if (localName.equals("conn") || localName.equals("stmt")) {
@@ -154,6 +182,10 @@ public class JdbcLogCallback extends CallbackSkeleton {
 	}
 
 	public void stopwatchStop(Split split) {
+		if (!initialized) {
+			initialize();
+		}
+
 		String fullName = split.getStopwatch().getName();
 		logger.log(Level.INFO, null, new Object[] {fullName, "stop", split.runningFor(), split.getStopwatch().getNote()});
 	}
