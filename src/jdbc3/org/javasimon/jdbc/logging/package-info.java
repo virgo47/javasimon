9a4c17b31bb49148917d677520eaca56d1fb17ae
radovan.sninsky@gmail.com <radovan.sninsky@gmail.com@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2009-04-16 14:38:05 +0000

    Javadoc for package org.javasimon.jdbc.logging.
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@196 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- src/jdbc3/org/javasimon/jdbc/logging/package-info.java
+++ src/jdbc3/org/javasimon/jdbc/logging/package-info.java
@@ -1,4 +1,42 @@
 /**
  * Logging feature of Simon JDBC Proxy driver.
+ * <p>
+ * This logging feature is based on Callback feature of Javasimon core library, see {@link org.javasimon.Callback}.
+ * I short, Callback feature allow intercept event from simon (like start, stop, etc.) and perform custom action,
+ * in this case log message with details into file or to console.
+ * <p>
+ * Java standard logging feature ({@link java.util.logging) is used for logging from Simon JDBC Proxy driver. There
+ * are three targets where to log and also three ways how to setup logging.
+ * Where to log:
+ * <ul>
+ * <li>
+ * <b>to console</b> - log messages are routed through standard {@link java.util.logging.ConsoleHandler} so messages
+ * are printed to error output (or somewhere where an output is redirected)
+ * </li>
+ * <li>
+ * <b>to logger</b>< - logger is provided by name, all messages are then logged by obtained
+ * {@link java.util.logging.Logger}
+ * </li>
+ * <li>
+ * <b>to file<b> - filename is provided, log messages are routed through standard {@link java.util.logging.FileHandler}
+ * </li>
+ * </ul>
+ * Log messages are by default formated to human readable form by build in human formatter (identificator: HUMAN).
+ * There is also other build-in csv formatter (identifier CSV). There is also possibility to provide custom formatter.
+ * Only requriment is that custom formatter extends from {@link org.javasimon.jdbc.logging.SimonFormatter}.
+ * <br>
+ * How to setup jdbc logging:
+ * <ul>
+ * <li>
+ * <b>by jdbc url</b> - simon jdbc url can by enhanced with simon specific parameters for setup logging to file, logger
+ * or console and format of log messages. For details about simon url specific parametars see
+ * {@link org.javasimon.jdbc.Driver}.
+ * </li>
+ * <li><b>by property file</b> - ...</li>
+ * <li>
+ * <b>by jmx</b> - jmx package offers {@link org.javasimon.jmx.JdbcMXBean} that provides methods for starting logging
+ * to file, logger or console and for stopping too. Each method has its parameters to setup specific details.
+ * </li>
+ * </ul>
  */
 package org.javasimon.jdbc.logging;
