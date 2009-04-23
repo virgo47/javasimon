5c82525bed2cf023d5975e2a08a6fd367ac40d36
virgo47 <virgo47@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2009-04-23 07:05:30 +0000

    structure changes towards maven, but maven fails on jdk15/16 issue, ant still working ;-)
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@201 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- jdbc3/src/test/java/org/javasimon/jdbc/SqlNormalizerTestNG.java
+++ jdbc3/src/test/java/org/javasimon/jdbc/SqlNormalizerTestNG.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
  *
  * @author Radovan Sninsky
  * @author <a href="mailto:virgo47@gmail.com">Richard "Virgo" Richter</a>
- * @version $Revision$ $Date$
+ * @version $Revision: 192 $ $Date: 2009-03-22 01:03:48 +0100 (Sun, 22 Mar 2009) $
  * @created 25.8.2008 10:51:52
  * @since 1.0
  */
