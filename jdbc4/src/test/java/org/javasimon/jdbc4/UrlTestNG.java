7e079a0fd3b281bd033f185a4cf04721eafced86
Richard 'Virgo' Richter <virgo47@gmail.com>
2011-02-19 12:50:10 +0000

    eval switched for MVEL2, spring version pushed to 3.0.5

--- jdbc4/src/test/java/org/javasimon/jdbc4/UrlTestNG.java
+++ jdbc4/src/test/java/org/javasimon/jdbc4/UrlTestNG.java
@@ -17,9 +17,10 @@ public class UrlTestNG {
 	public Object[][] createTestData() {
 		return new Object[][] {
 			{"jdbc:simon:sqlserver://test;databaseName=testdb", "jdbc:sqlserver://test;databaseName=testdb"},
-			{"jdbc:simon:sqlserver://test;databaseName=testdb;simon_format=human;simon_console=t",
-					"jdbc:sqlserver://test;databaseName=testdb"},
-			{"jdbc:simon:h2:tcp://localhost:6762/testdb;simon_console=y", "jdbc:h2:tcp://localhost:6762/testdb"}
+			// TODO: I'm not sure if I want to support this in JDBC4 in the future or how to properly configure this all
+//			{"jdbc:simon:sqlserver://test;databaseName=testdb;simon_format=human;simon_console=t",
+//					"jdbc:sqlserver://test;databaseName=testdb"},
+//			{"jdbc:simon:h2:tcp://localhost:6762/testdb;simon_console=y", "jdbc:h2:tcp://localhost:6762/testdb"}
 		};
 	}
 
