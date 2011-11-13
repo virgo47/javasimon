5f9c85a114244e01d1ba2c2c85e523b11f33923c
virgo47 <virgo47@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2011-11-13 22:01:30 +0000

    added BenchmarkUtils and little javadoc fixes, mostly added @since
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@364 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- examples/src/main/java/org/javasimon/examples/NanoMillisComparison.java
+++ examples/src/main/java/org/javasimon/examples/NanoMillisComparison.java
@@ -1,10 +1,8 @@
 package org.javasimon.examples;
 
-import org.javasimon.Stopwatch;
 import org.javasimon.SimonManager;
-import org.javasimon.Simon;
-import org.javasimon.Split;
-import org.javasimon.utils.*;
+import org.javasimon.Stopwatch;
+import org.javasimon.utils.BenchmarkUtils;
 
 /**
  * Various timer calles compared along with stopwatch start/stop calls.
@@ -13,13 +11,6 @@ import org.javasimon.utils.*;
  */
 public final class NanoMillisComparison {
 	private static final int LOOP = 10000000;
-
-	private static Stopwatch nanos;
-	private static Stopwatch empty;
-	private static Stopwatch millis;
-	private static Stopwatch assignMs;
-	private static Stopwatch stopwatch;
-	private static Stopwatch assignNs;
 
 	private NanoMillisComparison() {
 	}
@@ -30,84 +21,75 @@ public final class NanoMillisComparison {
 	 * @param args command line arguments
 	 */
 	public static void main(String[] args) {
-		AbstractDataCollector collector = null;
-		for (int round = 1; round <= 5; round++) {
-			System.out.println("\nRound: " + round);
-			emptyTest();
-			millisTest();
-			nanoTest();
-			msAssignTest();
-			nsAssignTest();
-			simonUsage();
-			if (collector == null) {
-				 collector = new AbstractDataCollector(empty, millis, nanos, assignMs, assignNs, stopwatch) {
-					 public double obtainValue(Simon simon) {
-						 return ((Stopwatch) simon).getTotal();
-					 }
-				 };
+		BenchmarkUtils.run(2, 5,
+			new BenchmarkUtils.Task("empty") {
+				@Override
+				public void perform() throws Exception {
+					for (int i = 0; i < LOOP; i++) {
+					}
+				}
+			},
+			new BenchmarkUtils.Task("millis") {
+				@Override
+				public void perform() throws Exception {
+					for (int i = 0; i < LOOP; i++) {
+						System.currentTimeMillis();
+					}
+				}
+			},
+			new BenchmarkUtils.Task("nanos") {
+				@Override
+				public void perform() throws Exception {
+					for (int i = 0; i < LOOP; i++) {
+						System.nanoTime();
+					}
+				}
+			},
+			new BenchmarkUtils.Task("assign-ms") {
+				@Override
+				public void perform() throws Exception {
+					for (int i = 0; i < LOOP; i++) {
+						long ms = System.currentTimeMillis();
+					}
+				}
+			},
+			new BenchmarkUtils.Task("assign-ns") {
+				@Override
+				public void perform() throws Exception {
+					for (int i = 0; i < LOOP; i++) {
+						long ns = System.nanoTime();
+					}
+				}
+			},
+			new BenchmarkUtils.Task("stopwatch") {
+				@Override
+				public void perform() throws Exception {
+					Stopwatch simon = SimonManager.getStopwatch(null);
+					for (int i = 0; i < LOOP; i++) {
+						simon.start().stop();
+					}
+				}
+			},
+			new BenchmarkUtils.Task("stopwatch-manager") {
+				@Override
+				public void perform() throws Exception {
+					for (int i = 0; i < LOOP; i++) {
+						SimonManager.getStopwatch("some.name").start().stop();
+					}
+				}
 			}
-			collector.collect();
-		}
-		System.out.println("\nGoogle Chart:\n" + GoogleChartGenerator.barChart(collector, "10M-loop duration", 1000000, "ms"));
-	}
+		);
 
-	private static void emptyTest() {
-		empty = SimonManager.getStopwatch("empty").reset();
-		Split split = empty.start();
-		for (int i = 0; i < LOOP; i++) {
-		}
-		split.stop();
-		System.out.println("Empty: " + empty);
-	}
-
-	private static void millisTest() {
-		millis = SimonManager.getStopwatch("millis").reset();
-		Split split = millis.start();
-		for (int i = 0; i < LOOP; i++) {
-			System.currentTimeMillis();
-		}
-		split.stop();
-		System.out.println("Millis: " + millis);
-	}
-
-	private static void nanoTest() {
-		nanos = SimonManager.getStopwatch("nanos").reset();
-		Split split = nanos.start();
-		for (int i = 0; i < LOOP; i++) {
-			System.nanoTime();
-		}
-		split.stop();
-		System.out.println("Nanos: " + nanos);
-	}
-
-	private static void msAssignTest() {
-		assignMs = SimonManager.getStopwatch("assign-ms").reset();
-		Split split = assignMs.start();
-		for (int i = 0; i < LOOP; i++) {
-			long ms = System.currentTimeMillis();
-		}
-		split.stop();
-		System.out.println("Assign ms: " + assignMs);
-	}
-
-	private static void nsAssignTest() {
-		assignNs = SimonManager.getStopwatch("assign-ns").reset();
-		Split split = assignNs.start();
-		for (int i = 0; i < LOOP; i++) {
-			long ns = System.nanoTime();
-		}
-		split.stop();
-		System.out.println("Assign ns: " + assignNs);
-	}
-
-	private static void simonUsage() {
-		Stopwatch simon = SimonManager.getStopwatch(null);
-		stopwatch = SimonManager.getStopwatch("stopwatch").reset();
-		Split split = stopwatch.start();
-		for (int i = 0; i < LOOP; i++) {
-			simon.start().stop();
-		}
-		split.stop();
-		System.out.println("Stopwatch start/stop: " + stopwatch);
+		// TODO: add the following to BenchmarkUitls as well
+//		AbstractDataCollector collector = null;
+//			if (collector == null) {
+//				 collector = new AbstractDataCollector(empty, millis, nanos, assignMs, assignNs, stopwatch) {
+//					 public double obtainValue(Simon simon) {
+//						 return ((Stopwatch) simon).getTotal();
+//					 }
+//				 };
+//			}
+//			collector.collect();
+//		System.out.println("\nGoogle Chart:\n" + GoogleChartGenerator.barChart(collector, "10M-loop duration", 1000000, "ms"));
 	}
 }
