265234c36829f9ad0c6759061e4a2d603074a485
Richard 'Virgo' Richter <virgo47@gmail.com>
2011-12-27 20:59:23 +0000

    little fixes/javadoc after filter-rule refactor + I forgot to exclude Gerald's json/jquery console, so the module was checked in in the previous commit :-) "console"

--- core/src/main/java/org/javasimon/callback/FilterCallback.java
+++ core/src/main/java/org/javasimon/callback/FilterCallback.java
@@ -2,18 +2,7 @@ package org.javasimon.callback;
 
 /**
  * FilterCallback extends Callback with filtering capabilities. Filter callback
- * adds filtering rules that allow selective event propagation to children.
- * <p/>
- * Rule can be one of the following types:
- * <ul>
- * <li>must - rule MUST be true and following rules are checked
- * <li>suffice - if this rule is true the filter passes the event to children
- * otherwise next rules are checked
- * <li>must-not - if this rule is true the filter ignores the event, otherwise
- * next rules are checked
- * </ul>
- * As the order is important not all MUST rules must pass if there is any
- * satisfied SUFFICE rule before.
+ * adds {@link FilterRule}s that allow selective event propagation to children.
  *
  * @author <a href="mailto:virgo47@gmail.com">Richard "Virgo" Richter</a>
  */
