38f0d691c8479b09dcfd6fe1c81da0cd9eab61b3
virgo47 <virgo47@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2012-01-04 13:57:42 +0000

    fixed version for ant build (3.1.1.), little fixes in package javadocs, SimonServletFilter has protected getThreshold (in ns)
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@417 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- examples/src/main/java/org/javasimon/examples/package-info.java
+++ examples/src/main/java/org/javasimon/examples/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Basic examples of Java Simon usage - mostly for {@link org.javasimon.Stopwatch}, {@link org.javasimon.Callback},
+ * Basic examples of Java Simon usage - mostly for {@link org.javasimon.Stopwatch}, {@link org.javasimon.callback.Callback},
  * {@link org.javasimon.Sample}, some multithreading, benchmarks.
  */
 package org.javasimon.examples;
