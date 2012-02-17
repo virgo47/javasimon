ad0584362e8843d6fef11c12d2b20fa54273fed3
virgo47 <virgo47@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2012-02-17 22:12:12 +0000

    EJB interceptor reworked into more general one
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@463 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- javaee/src/main/java/org/javasimon/javaee/package-info.java
+++ javaee/src/main/java/org/javasimon/javaee/package-info.java
@@ -1,9 +1,9 @@
 /**
- * Java EE support for Simons contains Java EE components (servlet filter and EJB interceptor) that can be used or extended.
+ * Java EE support for Simons contains Java EE components (servlet filter and EJB/CDI interceptor) that can be used or extended.
  * Package contains following components:
  * <ul>
  * <li>{@link org.javasimon.javaee.SimonServletFilter} is Servlet Filter for monitoring HTTP requests.</li>
- * <li>{@link org.javasimon.javaee.SimonEjbInterceptor} is EJB Interceptor for monitoring EJB method invocations.</li>
+ * <li>{@link SimonInterceptor} is EJB/CDI Interceptor for monitoring business method invocations.</li>
  * </ul>
  * Consult class javadocs for usage information.
  */
