2c13e2c7ed6d64058e24ef97b34db7f7e32ed618
virgo47 <virgo47@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2012-03-07 16:58:10 +0000

    javadoc adjustments - using <pre> and @literal - better to read in code + update of Ant's build.xml to newer deps
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@470 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- spring/src/main/java/org/javasimon/spring/ManagerFactoryBean.java
+++ spring/src/main/java/org/javasimon/spring/ManagerFactoryBean.java
@@ -2,6 +2,7 @@ package org.javasimon.spring;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.javasimon.Manager;
 import org.javasimon.SimonManager;
 import org.javasimon.SwitchingManager;
@@ -11,25 +12,26 @@ import org.springframework.beans.factory.FactoryBean;
 /**
  * Factory bean which produces Simon manager.
  * Spring configuration:
- * <code><pre>
- *	&lt;bean id="jmxServer" class="org.springframework.jmx.support.MBeanServerFactoryBean"&gt;
- *		&lt;property name="locateExistingServerIfPossible" value="true"/&gt;
- *	&lt;/bean&gt;
- *	&lt;bean id="simonManager" class="org.javasimon.spring.ManagerFactoryBean"&gt;
- *		&lt;property name="callbacks"&gt;
- *			&lt;list&gt;
- *				&lt;bean class="org.javasimon.jmx.JmxRegisterCallback"&gt;
- *					&lt;constructor-arg ref="jmxServer"/&gt;
- *					&lt;constructor-arg value="org.javasimon"/&gt;
- *				&lt;/bean&gt;
- *				&lt;bean class="org.javasimon.utils.LoggingCallback"/&gt;
- *			&lt;/list&gt;
- *		&lt;/property&gt;
- *	&lt;/bean&gt;
- *	&lt;bean id="simonInterceptor" class="org.javasimon.spring.MonitoringInterceptor"&gt;
- *		&lt;property name="manager" ref="simonManager"/&gt;
- *	&lt;/bean&gt;
- * </pre></code>
+ * <pre>{@literal
+ * <bean id="jmxServer" class="org.springframework.jmx.support.MBeanServerFactoryBean">
+ *     <property name="locateExistingServerIfPossible" value="true"/>
+ * </bean>
+ * <bean id="simonManager" class="org.javasimon.spring.ManagerFactoryBean">
+ *     <property name="callbacks">
+ *         <list>
+ *             <bean class="org.javasimon.jmx.JmxRegisterCallback">
+ *                 <constructor-arg ref="jmxServer"/>
+ *                 <constructor-arg value="org.javasimon"/>
+ *             </bean>
+ *             <bean class="org.javasimon.utils.LoggingCallback"/>
+ *         </list>
+ *     </property>
+ * </bean>
+ * <bean id="simonInterceptor" class="org.javasimon.spring.MonitoringInterceptor">
+ *     <property name="manager" ref="simonManager"/>
+ * </bean>}
+ * </pre>
+ *
  * @author gquintana
  */
 public class ManagerFactoryBean implements FactoryBean<Manager> {
@@ -37,29 +39,31 @@ public class ManagerFactoryBean implements FactoryBean<Manager> {
 	 * Flag indicating whether Simon manager singleton should be used.
 	 * Defaults to true.
 	 */
-	private boolean singleton=true;
+	private boolean singleton = true;
 	/**
 	 * List of callbacks to add to the manager
 	 */
-	private List<Callback> callbacks=new ArrayList<Callback>();
+	private List<Callback> callbacks = new ArrayList<Callback>();
 	/**
 	 * Flag indicated whether Simon manager should enabled or not.
 	 * Defaults to true.
 	 */
-	private boolean enabled=true;
+	private boolean enabled = true;
+
 	/**
 	 * Get simon manager instance.
 	 * If singleton is enabled SimonManager.manager() is invoked else new
 	 * Manager is created.
 	 * Then callbacks are appended to this manager
+	 *
 	 * @return Simon manager
 	 */
 	public Manager getObject() throws Exception {
 		Manager manager;
 		if (singleton) {
-			manager=SimonManager.manager();
+			manager = SimonManager.manager();
 		} else {
-			manager=new SwitchingManager();
+			manager = new SwitchingManager();
 		}
 		registerCallbacks(manager);
 		configureEnabled(manager);
@@ -83,15 +87,17 @@ public class ManagerFactoryBean implements FactoryBean<Manager> {
 
 	/**
 	 * Register callbacks in given manager
+	 *
 	 * @param manager Manager
 	 */
 	private void registerCallbacks(Manager manager) {
-		if (callbacks!=null) {
-			for(Callback callback:callbacks) {
+		if (callbacks != null) {
+			for (Callback callback : callbacks) {
 				manager.callback().addCallback(callback);
 			}
 		}
 	}
+
 	public List<Callback> getCallbacks() {
 		return callbacks;
 	}
@@ -99,12 +105,14 @@ public class ManagerFactoryBean implements FactoryBean<Manager> {
 	public void setCallbacks(List<Callback> callbacks) {
 		this.callbacks = callbacks;
 	}
+
 	/**
 	 * When needed toggle the enabled flag of given Simon manager
+	 *
 	 * @param manager Simon manager
 	 */
 	private void configureEnabled(Manager manager) {
-		if (enabled!=manager.isEnabled()) {
+		if (enabled != manager.isEnabled()) {
 			if (enabled) {
 				manager.enable();
 			} else {
