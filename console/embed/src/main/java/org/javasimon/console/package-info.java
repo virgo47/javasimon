7216d573910f8c4b13c49bfe54bf7052b111a260
gerald.quintana@gmail.com <gerald.quintana@gmail.com@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2012-04-17 20:24:36 +0000

    Added missing package-infos
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@534 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

---
+++ console/embed/src/main/java/org/javasimon/console/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Java Simon embeddable Web console base package.
+ * Contains Front controller {@link SimonConsoleRequestProcessor} which is used
+ * by a Servlet {@link SimonConsoleServlet} and a Servlet Filter {@link SimonConsoleFilter}.
+ */
+package org.javasimon.console;
---
+++ console/embed/src/main/java/org/javasimon/console/reflect/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Reflection tools to dynamically extract information from Simons.
+ */
+package org.javasimon.console.reflect;
