d86d8c6dbd09e5d884949d474e39b42d3046402f
Richard 'Virgo' Richter <virgo47@gmail.com>
2012-04-24 08:58:04 +0000

    added more extension points to name creation in Http Source

--- javaee/src/main/java/org/javasimon/javaee/HttpStopwatchSource.java
+++ javaee/src/main/java/org/javasimon/javaee/HttpStopwatchSource.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
  * Used by {@link SimonServletFilter} as default stopwatch source.
  * Can be overriden to customize monitored HTTP Requests and their
  * related Simon name.
- *
+ * <p/>
  * To select which HTTP Request should be monitored method {@link #isMonitored} can be overriden. Default implementation monitors everything except for
  * typical resource-like requests (images, JS/CSS, ...).
  *
@@ -28,7 +28,7 @@ public class HttpStopwatchSource extends AbstractStopwatchSource<HttpServletRequ
 	public static final String DEFAULT_SIMON_PREFIX = "org.javasimon.web";
 
 	/**
-	 * Simon prefix (protected for subclass usage) - can be {@code null}.
+	 * Simon prefix, can be set to {@code null}.
 	 */
 	private String prefix = DEFAULT_SIMON_PREFIX;
 
@@ -57,20 +57,58 @@ public class HttpStopwatchSource extends AbstractStopwatchSource<HttpServletRequ
 
 	/**
 	 * Returns Simon name for the specified HTTP request with the specified prefix. By default it contains URI without parameters with
-	 * all slashes replaced for dots (slashes then determines position in Simon hierarchy). Method can be
-	 * overriden.
+	 * all slashes replaced for dots (slashes then determines position in Simon hierarchy). Method can NOT be overriden, but some of the
+	 * following steps can:
+	 * <ol>
+	 * <li>the request is transformed to the string ({@link #requestToStringForMonitorName(javax.servlet.http.HttpServletRequest)}, can be overriden),</li>
+	 * <li>the characters that are not allowed as part of the Simon name are replaced with underscore (_) - replacement regex can be changed with {@link #setReplaceUnallowed(String)},</li>
+	 * <li>any subsequent slashes and dots are replaced with a single dot ({@link org.javasimon.Manager#HIERARCHY_DELIMITER})</li>
+	 * </ol>
 	 *
 	 * @param request HTTP request
 	 * @return fully qualified name of the Simon
+	 * @see #requestToStringForMonitorName(javax.servlet.http.HttpServletRequest)
 	 */
-	protected String getMonitorName(HttpServletRequest request) {
-		String uri = request.getRequestURI();
-		uri = jsessionParameterReplacer.process(uri);
+	protected final String getMonitorName(HttpServletRequest request) {
+		String uri = requestToStringForMonitorName(request);
 		String localName = SimonServletFilterUtils.getSimonName(uri, unallowedCharacterReplacer);
 		if (prefix == null || prefix.isEmpty()) {
 			return localName;
 		}
 		return prefix + Manager.HIERARCHY_DELIMITER + localName;
+	}
+
+	/**
+	 * Performs the first step in getting the monitor name from the specified HTTP request - here any custom ignore logic should happen.
+	 * By default the name is URI (without parameters - see {@link javax.servlet.http.HttpServletRequest#getRequestURI()}) with JSessionID
+	 * removed (see {@link #removeJSessionIdFromUri(String)}). This method can be overriden for two typical reasons:
+	 * <ul>
+	 * <li>Name of the monitor (Stopwatch) should be based on something else then URI,</li>
+	 * <li>there are other parts of the name that should be modified or ignored (e.g., REST parameters that are part of the URI).</li>
+	 * </ul>
+	 *
+	 * @param request HTTP request
+	 * @return preprocessed URI that will be converted to the Simon name
+	 * @see #getMonitorName(javax.servlet.http.HttpServletRequest)
+	 * @see #removeJSessionIdFromUri(String)
+	 */
+	protected String requestToStringForMonitorName(HttpServletRequest request) {
+		String uri = request.getRequestURI();
+		uri = removeJSessionIdFromUri(uri);
+		return uri;
+	}
+
+	/**
+	 * Called by default implementation of {@link #requestToStringForMonitorName(javax.servlet.http.HttpServletRequest)} and extracted
+	 * so it can be used by any overriding implementation of the same method. Method can be overriden if the default behavior is not
+	 * sufficient.
+	 *
+	 * @param uri preprocessed URI that may contain JSessionID
+	 * @return preprocessed URI without JSessionID
+	 * @see #requestToStringForMonitorName(javax.servlet.http.HttpServletRequest)
+	 */
+	protected String removeJSessionIdFromUri(String uri) {
+		return jsessionParameterReplacer.process(uri);
 	}
 
 	/**
