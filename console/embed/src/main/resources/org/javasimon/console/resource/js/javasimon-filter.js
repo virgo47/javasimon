37775b788d9e0337d8e890fabeda1dcd09214c3e
Gerald Quintana <gerald.quintana@gmail.com>
2012-05-01 18:33:10 +0000

    Fixed Issue#78: pattern is validated with regexp, help is displayed in case of error Issue#74 comment: Remove "Auto" from time formats

--- console/embed/src/main/resources/org/javasimon/console/resource/js/javasimon-filter.js
+++ console/embed/src/main/resources/org/javasimon/console/resource/js/javasimon-filter.js
@@ -4,8 +4,9 @@ window.javasimon=javasimon;
 /**
  * Filter controller
  */
-javasimon.FilterController=function(oPatternText, oTypeSelect, oTimeFormatSelect) {
+javasimon.FilterController=function(oPatternText, oPatternHelp, oTypeSelect, oTimeFormatSelect) {
 	this.oPatternText=oPatternText;
+	this.oPatternHelp=oPatternHelp;
 	this.oTypeSelect=oTypeSelect;
 	this.oTimeFormatSelect=oTimeFormatSelect;
 };
@@ -32,18 +33,44 @@ javasimon.FilterController.prototype={
 			sTimeFormat:sTimeFormat||""
 		};
 	},
+	arePattern:[
+		new RegExp("^[\\*]?[-_\\[\\]A-Za-z0-9.,@$%()<>]+[\\*]?$"),
+		new RegExp("^[-_\\[\\]A-Za-z0-9.,@$%()<>]+[\\*]?[-_\\[\\]A-Za-z0-9.,@$%()<>]+$")
+	],
+	fnValidate:function() {
+		var sPattern=this.oPatternText.val();
+		var bPatternValid=!sPattern // Empty or undefined is OK
+			||this.arePattern[0].test(sPattern) // Starting and/or ending with *
+			||this.arePattern[1].test(sPattern); // Containing single *
+		if (bPatternValid) {
+			// Valid pattern
+			$(this.oPatternText).removeClass("error");
+			$(this.oPatternHelp).addClass("hidden");
+		} else {
+			// Invalid pattern
+			$(this.oPatternText).addClass("error");
+			$(this.oPatternHelp).removeClass("hidden");
+		}
+		return bPatternValid;
+	},
 	fnExportCsv:function() {
-		javasimon.TableService.fnGetDataAsCsv(this.fnGetVal());
+		if (this.fnValidate()) {
+			javasimon.TableService.fnGetDataAsCsv(this.fnGetVal());
+		}
 	},
 	fnExportHtml:function() {
-		javasimon.TableService.fnGetDataAsHtml(this.fnGetVal());
+		if (this.fnValidate()) {
+			javasimon.TableService.fnGetDataAsHtml(this.fnGetVal());
+		}
 	},
 	fnResetAll:function(fnAjaxCallback) {
-		var oFilterVal=this.fnGetVal();
-		var oData={
-			pattern:oFilterVal.sPattern,
-			type:oFilterVal.sType
-		};
-		javasimon.ResetService.fnResetAll(oData, fnAjaxCallback);
+		if (this.fnValidate()) {
+			var oFilterVal=this.fnGetVal();
+			var oData={
+				pattern:oFilterVal.sPattern,
+				type:oFilterVal.sType
+			};
+			javasimon.ResetService.fnResetAll(oData, fnAjaxCallback);
+		}
 	}
 };
