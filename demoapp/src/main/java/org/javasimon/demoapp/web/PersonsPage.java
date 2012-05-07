f467048d76cc9bc64915dd8260049778d956b449
virgo47 <virgo47@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2012-05-07 14:40:53 +0000

    little link in demoapp to create new random stopwatch
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@570 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- demoapp/src/main/java/org/javasimon/demoapp/web/PersonsPage.java
+++ demoapp/src/main/java/org/javasimon/demoapp/web/PersonsPage.java
@@ -1,14 +1,50 @@
 package org.javasimon.demoapp.web;
 
+import java.util.Random;
+
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.util.string.*;
+import org.javasimon.Manager;
 import org.javasimon.SimonManager;
+import org.javasimon.Split;
 
 public class PersonsPage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
+	private static final String[] RANDOM_NAMES = ("Deadra Breakell Rod Herrero Genesis Boutilier Cliff Daus Carey Chevas" +
+		" Loralee Rizvi Virgen Pahler Un Muscat Elwood Poeppel Jeffry Carlise Kuramoto Bibi Whatcott Lianne Tellefson" +
+		" Ruthanne Stipes Elwood Kisselburg Raphael Maxam Pura Abrecht Rod Jernberg Bok Mehrtens Brittanie Palamino" +
+		" Jeffry Wansing Delsie Palms Rob Doub Moises Minney Armand Khaleel").split(" ");
+
+	private static final Random RANDOM = new Random();
+
+	@SuppressWarnings("UnusedParameters")
 	public PersonsPage(final PageParameters parameters) {
-		SimonManager.getCounter("some.counter").increase(2);
-		SimonManager.getCounter("some.counter").decrease();
+		Split split = null;
+		StringValue parameterNew = parameters.get("new");
+		if (parameterNew != null && !parameterNew.isEmpty()) {
+			split = SimonManager.getStopwatch(generateRandomName(5)).start();
+		}
+		try {
+			SimonManager.getCounter("some.counter").increase(2);
+			SimonManager.getCounter("some.counter").decrease();
+		} finally {
+			if (split != null) {
+				split.stop();
+			}
+		}
 	}
+
+	private static String generateRandomName(int depth) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < depth; i++) {
+			if (sb.length() > 0) {
+				sb.append(Manager.HIERARCHY_DELIMITER);
+			}
+			sb.append(RANDOM_NAMES[RANDOM.nextInt(RANDOM_NAMES.length)]);
+		}
+		return sb.toString();
+	}
+
 }
