03c70380bf4e873baf3816e7e2e5e3dbc6e38fc8
virgo47 <virgo47@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2012-05-11 22:00:30 +0000

    fixed minor javadoc link problem
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@591 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- javaee/src/main/java/org/javasimon/javaee/reqreporter/package-info.java
+++ javaee/src/main/java/org/javasimon/javaee/reqreporter/package-info.java
@@ -1,4 +1,4 @@
 /**
- * Package contains classes reporting slow HTTP requests - interface {@link RequestReporter} and various (default) implementations.
+ * Package contains classes reporting slow HTTP requests - interface {@link org.javasimon.javaee.reqreporter.RequestReporter} and various (default) implementations.
  */
 package org.javasimon.javaee.reqreporter;
