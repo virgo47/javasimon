7e4852dce05039794e3354f2c0b9d3f3f87565c1
gerald.quintana@gmail.com <gerald.quintana@gmail.com@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2012-05-21 16:54:32 +0000

    Fixed Issue#85 . Thanks RossbachP for Patch.
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@594 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- console/embed/src/test/java/org/javasimon/console/action/TableHtmlActionTest.java
+++ console/embed/src/test/java/org/javasimon/console/action/TableHtmlActionTest.java
@@ -25,10 +25,9 @@ public class TableHtmlActionTest {
 		action.execute();
 		assertEquals(context.getContentType(), "text/html");
 		String html = context.toString();
-		assertTrue(html.contains("<td class=\"name\">B</td><td class=\"type\">STOPWATCH</td><td class=\"counter\">2</td><td class=\"total\">300</td><td class=\"min\">100</td><td class=\"mean\">150</td><td class=\"last\">100</td><td class=\"max\">200</td><td class=\"standardDeviation\">50</td>"));
-		assertTrue(html.contains("<td class=\"name\">A</td><td class=\"type\">STOPWATCH</td><td class=\"counter\">3</td><td class=\"total\">600</td><td class=\"min\">100</td><td class=\"mean\">200</td><td class=\"last\">300</td><td class=\"max\">300</td><td class=\"standardDeviation\">82</td>"));
-		assertTrue(html.contains("<td class=\"name\">C</td><td class=\"type\">STOPWATCH</td><td class=\"counter\">1</td><td class=\"total\">300</td><td class=\"min\">300</td><td class=\"mean\">300</td><td class=\"last\">300</td><td class=\"max\">300</td><td class=\"standardDeviation\">0</td>"));
-
+		assertTrue(html.contains("<td class=\"name\">B</td><td class=\"type\"><img class=\"stopwatch icon\" src=\"../../resource/images/TypeStopwatch.png\" alt=\"stopwatch\" />STOPWATCH</td><td class=\"counter\">2</td><td class=\"total\">300</td><td class=\"min\">100</td><td class=\"mean\">150</td><td class=\"last\">100</td><td class=\"max\">200</td><td class=\"standardDeviation\">50</td>"));
+		assertTrue(html.contains("<td class=\"name\">A</td><td class=\"type\"><img class=\"stopwatch icon\" src=\"../../resource/images/TypeStopwatch.png\" alt=\"stopwatch\" />STOPWATCH</td><td class=\"counter\">3</td><td class=\"total\">600</td><td class=\"min\">100</td><td class=\"mean\">200</td><td class=\"last\">300</td><td class=\"max\">300</td><td class=\"standardDeviation\">82</td>"));
+		assertTrue(html.contains("<td class=\"name\">C</td><td class=\"type\"><img class=\"stopwatch icon\" src=\"../../resource/images/TypeStopwatch.png\" alt=\"stopwatch\" />STOPWATCH</td><td class=\"counter\">1</td><td class=\"total\">300</td><td class=\"min\">300</td><td class=\"mean\">300</td><td class=\"last\">300</td><td class=\"max\">300</td><td class=\"standardDeviation\">0</td>"));
 	}
 
 	@Test
@@ -40,9 +39,9 @@ public class TableHtmlActionTest {
 		action.execute();
 		assertEquals(context.getContentType(), "text/html");
 		String html = context.toString();
-		assertTrue(html.contains("<td class=\"name\">B</td><td class=\"type\">STOPWATCH</td><td class=\"counter\">2</td><td class=\"total\">300 ms</td><td class=\"min\">100 ms</td><td class=\"mean\">150 ms</td><td class=\"last\">100 ms</td><td class=\"max\">200 ms</td><td class=\"standardDeviation\">50.0 ms</td>"));
-		assertTrue(html.contains("<td class=\"name\">A</td><td class=\"type\">STOPWATCH</td><td class=\"counter\">3</td><td class=\"total\">600 ms</td><td class=\"min\">100 ms</td><td class=\"mean\">200 ms</td><td class=\"last\">300 ms</td><td class=\"max\">300 ms</td><td class=\"standardDeviation\">81.6 ms</td>"));
-		assertTrue(html.contains("<td class=\"name\">C</td><td class=\"type\">STOPWATCH</td><td class=\"counter\">1</td><td class=\"total\">300 ms</td><td class=\"min\">300 ms</td><td class=\"mean\">300 ms</td><td class=\"last\">300 ms</td><td class=\"max\">300 ms</td><td class=\"standardDeviation\">0</td>"));
+		assertTrue(html.contains("<td class=\"name\">B</td><td class=\"type\"><img class=\"stopwatch icon\" src=\"../../resource/images/TypeStopwatch.png\" alt=\"stopwatch\" />STOPWATCH</td><td class=\"counter\">2</td><td class=\"total\">300 ms</td><td class=\"min\">100 ms</td><td class=\"mean\">150 ms</td><td class=\"last\">100 ms</td><td class=\"max\">200 ms</td><td class=\"standardDeviation\">50.0 ms</td>"));
+		assertTrue(html.contains("<td class=\"name\">A</td><td class=\"type\"><img class=\"stopwatch icon\" src=\"../../resource/images/TypeStopwatch.png\" alt=\"stopwatch\" />STOPWATCH</td><td class=\"counter\">3</td><td class=\"total\">600 ms</td><td class=\"min\">100 ms</td><td class=\"mean\">200 ms</td><td class=\"last\">300 ms</td><td class=\"max\">300 ms</td><td class=\"standardDeviation\">81.6 ms</td>"));
+		assertTrue(html.contains("<td class=\"name\">C</td><td class=\"type\"><img class=\"stopwatch icon\" src=\"../../resource/images/TypeStopwatch.png\" alt=\"stopwatch\" />STOPWATCH</td><td class=\"counter\">1</td><td class=\"total\">300 ms</td><td class=\"min\">300 ms</td><td class=\"mean\">300 ms</td><td class=\"last\">300 ms</td><td class=\"max\">300 ms</td><td class=\"standardDeviation\">0</td>"));
 
 	}
 }
