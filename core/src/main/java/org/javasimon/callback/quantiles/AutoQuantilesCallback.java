c3d00d610289c55c2ea823eef684e2fe8be3380c
Gerald Quintana <gerald.quintana@gmail.com>
2012-08-24 20:56:15 +0000

    Split QuantilesCallback in 2 Made QunatilesCallback more configurable

---
+++ core/src/main/java/org/javasimon/callback/quantiles/AutoQuantilesCallback.java
@@ -0,0 +1,214 @@
+package org.javasimon.callback.quantiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.javasimon.Simon;
+import org.javasimon.Split;
+import org.javasimon.Stopwatch;
+
+import org.javasimon.utils.SimonUtils;
+
+/**
+ * Callback which stores data in buckets to compute quantiles.
+ * Quantiles can only be obtained after warmup period, after which buckets are
+ * initialized. 
+ * For each Simon the following lifecycle occurs:<ol>
+ * <li><em>Warm up</em>:<ul>
+ * <li>Buckets do not exist</li>
+ * <li>Quantiles can not be computed</li>
+ * <li>Splits are kept</li>
+ * </ul>
+ * <li><em>Trigger</em>: splits count >= warmup count threshold<ul>
+ * <li>Buckets are created, configuration (min, max) is determined from kept splits, bucket number is constant</li>
+ * <li>Buckets are filled with previously stored splits</li>
+ * <li>Retained splits are removed</li>
+ * <li>From now on, quantiles can not be computed and splits are not kept anymore</li>
+ * </ul>
+ * <li><em>Normal</em>: <ul>
+ * <li>Buckets are filled/updated with new splits as they come</li>
+ * <li>Quantiles can be computed (provided there is enough splits and buckets are propertly configured)</li>
+ * </ul>
+ * </li></ol>
+ *
+ * @author gquintana
+ * @since 3.2
+ * @see Buckets
+ */
+public class AutoQuantilesCallback extends QuantilesCallback {
+
+	/**
+	 * Simon attribute name of the list of split values stored in Simons before warmup time.
+	 */
+	public static final String ATTR_NAME_BUCKETS_VALUES = "bucketsValues";
+
+	/**
+	 * Number of splits before buckets are initialized.
+	 * Default 10
+	 */
+	private final long warmupCounter;
+
+	/**
+	 * Number of buckets of data for each Simon.
+	 */
+	private final int bucketNb;
+
+	/**
+	 * Default constructor.
+	 */
+	public AutoQuantilesCallback() {
+		this.warmupCounter = 10;
+		this.bucketNb = 8;
+	}
+
+	/**
+	 * Constructor with warmup counter and number of buckets for each Simon.
+	 *
+	 * @param warmupCounter
+	 * @param bucketNb
+	 */
+	public AutoQuantilesCallback(long warmupCounter, int bucketNb) {
+		this.warmupCounter = warmupCounter;
+		this.bucketNb = bucketNb;
+	}
+
+	/**
+	 * Get the bucket values attribute or create it if it does not exist.
+	 */
+	@SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
+	private List<Long> getOrCreateBucketsValues(final Stopwatch stopwatch) {
+		synchronized (stopwatch) {
+			List<Long> values = getBucketsValues(stopwatch);
+			if (values == null) {
+				values = new ArrayList<Long>((int) warmupCounter);
+				stopwatch.setAttribute(ATTR_NAME_BUCKETS_VALUES, values);
+			}
+			return values;
+		}
+	}
+
+	/**
+	 * Get the bucket values attribute.
+	 */
+	@SuppressWarnings("unchecked")
+	private List<Long> getBucketsValues(final Stopwatch stopwatch) {
+		return (List<Long>) stopwatch.getAttribute(ATTR_NAME_BUCKETS_VALUES);
+	}
+
+
+	/**
+	 * Remove the bucket values attribute (after warmup).
+	 */
+	private void removeBucketsValues(final Stopwatch stopwatch) {
+		stopwatch.removeAttribute(ATTR_NAME_BUCKETS_VALUES);
+	}
+
+	/**
+	 * Create the buckets after warmup time.
+	 * Can be overriden to customize buckets configuration.
+	 * By default buckets are create with:<ul>
+	 * <li>Min: stopwatch min-10% rounded to inferior millisecond</li>
+	 * <li>Max: stopwatch max+10 rounded to superior millisecond</li>
+	 * <li>Nb buckets: {@link #bucketNb}
+	 *
+	 * @param stopwatch Stopwatch (containing configuration)
+	 * @return new Buckets objects
+	 */
+	protected Buckets createBucketsAfterWarmup(Stopwatch stopwatch) {
+		// Compute min
+		long min = stopwatch.getMin() * 90L / 100L; // min -10%
+		min = Math.max(0, min); // no negative mins
+		min = (min / SimonUtils.NANOS_IN_MILLIS) * SimonUtils.NANOS_IN_MILLIS; // round to lower millisecond
+		// Compute max
+		long max = (stopwatch.getMax() * 110L) / 100L; // max +10%
+		max = (max / SimonUtils.NANOS_IN_MILLIS + 1) * SimonUtils.NANOS_IN_MILLIS; // round to upper millisecond
+		Buckets buckets = createBuckets(stopwatch, min, max, bucketNb);
+		return buckets;
+	}
+	/**
+	 * When warmup ends, buckets are create and retained splits are
+	 * sorted in the buckets.
+	 */
+	protected final Buckets createBuckets(Stopwatch stopwatch) {
+		if (stopwatch.getCounter() > warmupCounter) {
+			Buckets buckets = createBucketsAfterWarmup(stopwatch);
+			// Add retained splits to buckets
+			buckets.addValues(getBucketsValues(stopwatch));
+			removeBucketsValues(stopwatch);
+			return buckets;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * When simon is created, the list containing Split values is added to stopwatch attributes.
+	 */
+	@Override
+	public void onSimonCreated(Simon simon) {
+		if (simon instanceof Stopwatch) {
+			Stopwatch stopwatch = (Stopwatch) simon;
+			getOrCreateBucketsValues(stopwatch);
+		}
+	}
+
+	/**
+	 * Called when there is a new split on a Stopwatch, either
+	 * {@link #onStopwatchStop} or {@link #onStopwatchAdd}.
+	 * If buckets have been initialized, the value is added to appropriate bucket.
+	 * Else if stopwatch is warming up value is added to value list.
+	 */
+	@Override
+	protected void onStopwatchSplit(Stopwatch stopwatch, Split split) {
+		Buckets buckets = getOrCreateBuckets(stopwatch);
+		long value = split.runningFor();
+		if (buckets == null) {
+			// Warming up
+			getOrCreateBucketsValues(stopwatch).add(value);
+		} else {
+			// Warm
+			buckets.addValue(value);
+			buckets.log(split);
+		}
+	}
+
+	/**
+	 * Called when there is a new split on a Stopwatch, either
+	 * {@link #onStopwatchStop} or {@link #onStopwatchAdd}.
+	 * If buckets have been initialized, the value is added to appropriate bucket.
+	 * Else if stopwatch is warming up value is added to value list.
+	 */
+	@Override
+	protected void onStopwatchSplit(Stopwatch stopwatch, long value) {
+		Buckets buckets = getOrCreateBuckets(stopwatch);
+		if (buckets == null) {
+			// Warming up
+			getOrCreateBucketsValues(stopwatch).add(value);
+		} else {
+			// Warm
+			buckets.addValue(value);
+		}
+	}
+	
+	/**
+	 * When the Stopwatch is reseted, so are the buckets.
+	 */
+	@Override
+	public void onSimonReset(Simon simon) {
+		if (simon instanceof Stopwatch) {
+			Stopwatch stopwatch = (Stopwatch) simon;
+			Buckets buckets = getBuckets(stopwatch);
+			if (buckets == null) {
+				// Cold
+				List<Long> values = getBucketsValues(stopwatch);
+				if (values != null) {
+					// Warming up
+					values.clear();
+				}
+			} else {
+				// Warm
+				buckets.clear();
+			}
+		}
+	}
+}
--- core/src/main/java/org/javasimon/callback/quantiles/QuantilesCallback.java
+++ core/src/main/java/org/javasimon/callback/quantiles/QuantilesCallback.java
@@ -1,7 +1,5 @@
 package org.javasimon.callback.quantiles;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.javasimon.Simon;
 import org.javasimon.Split;
@@ -12,55 +10,26 @@ import org.javasimon.callback.logging.LogTemplate;
 
 import static org.javasimon.callback.logging.LogTemplates.*;
 
-import org.javasimon.utils.SimonUtils;
 
 /**
- * Callback which stores data to compute quantiles.
- * Quantiles can only be obtained after warmup period, after which buckets are
- * initialized. Buckets are stored among simon attributes.
- * For each Simon the following lifecycle occurs:<ol>
- * <li><em>Warm up</em>:<ul>
- * <li>Buckets do not exist</li>
- * <li>Quantiles can not be computed</li>
- * <li>Splits are kept</li>
- * </ul>
- * <li><em>Trigger</em>: splits count >= warmup count threshold<ul>
- * <li>Buckets are created, configuration (min, max) is determined from kept splits, bucket number is constant</li>
- * <li>Buckets are filled with previously stored splits</li>
- * <li>Retained splits are removed</li>
- * <li>From now on, quantiles can not be computed and splits are not kept anymore</li>
- * </ul>
- * <li><em>Normal</em>: <ul>
- * <li>Buckets are filled/updated with new splits as they come</li>
- * <li>Quantiles can be computed (provided there is enough splits and buckets are propertly configured)</li>
- * </ul>
- * </li></ol>
- *
+ * Callback which stores data in buckets to compute quantiles.
+ * The {@link #createBuckets(org.javasimon.Stopwatch) should be
+ * implemented to configure the width and resolution of buckets. 
+ * Then {@link Buckets} are stored among Simon attributes.
+ * There are 2 implementations:
+ * <ul>
+ * <li>{@link AutoQuantilesCallback} tries to determine the best configuration for each Stopwatch.</li>
+ * <li>{@link FixedQuantilesCallback} uses a fixed configuration for all Stopwatches.</li>
+ * </ul> 
  * @author gquintana
  * @since 3.2
  * @see Buckets
  */
-public class QuantilesCallback extends CallbackSkeleton {
+public abstract class QuantilesCallback extends CallbackSkeleton {
 	/**
 	 * Simon attribute name of the buckets stored in Simons after warmup time.
 	 */
 	public static final String ATTR_NAME_BUCKETS = "buckets";
-
-	/**
-	 * Simon attribute name of the list of split values stored in Simons before warmup time.
-	 */
-	public static final String ATTR_NAME_BUCKETS_VALUES = "bucketsValues";
-
-	/**
-	 * Number of splits before buckets are initialized.
-	 * Default 10
-	 */
-	private final long warmupCounter;
-
-	/**
-	 * Number of buckets of data for each Simon.
-	 */
-	private final int bucketNb;
 
 	/**
 	 * Global flag indicating whether last splits should be logged once in a while.
@@ -75,51 +44,7 @@ public class QuantilesCallback extends CallbackSkeleton {
 	/**
 	 * Default constructor.
 	 */
-	public QuantilesCallback() {
-		this.warmupCounter = 10;
-		this.bucketNb = 8;
-	}
-
-	/**
-	 * Constructor with warmup counter and number of buckets for each Simon.
-	 *
-	 * @param warmupCounter
-	 * @param bucketNb
-	 */
-	public QuantilesCallback(long warmupCounter, int bucketNb) {
-		this.warmupCounter = warmupCounter;
-		this.bucketNb = bucketNb;
-	}
-
-	/**
-	 * Get the bucket values attribute or create it if it does not exist.
-	 */
-	@SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
-	private List<Long> getOrCreateBucketsValues(final Stopwatch stopwatch) {
-		synchronized (stopwatch) {
-			List<Long> values = getBucketsValues(stopwatch);
-			if (values == null) {
-				values = new ArrayList<Long>((int) warmupCounter);
-				stopwatch.setAttribute(ATTR_NAME_BUCKETS_VALUES, values);
-			}
-			return values;
-		}
-	}
-
-	/**
-	 * Get the bucket values attribute.
-	 */
-	@SuppressWarnings("unchecked")
-	private List<Long> getBucketsValues(final Stopwatch stopwatch) {
-		return (List<Long>) stopwatch.getAttribute(ATTR_NAME_BUCKETS_VALUES);
-	}
-
-
-	/**
-	 * Remove the bucket values attribute (after warmup).
-	 */
-	private void removeBucketsValues(final Stopwatch stopwatch) {
-		stopwatch.removeAttribute(ATTR_NAME_BUCKETS_VALUES);
+	protected QuantilesCallback() {
 	}
 
 	public boolean isLogEnabled() {
@@ -148,60 +73,47 @@ public class QuantilesCallback extends CallbackSkeleton {
 		}
 		return logTemplate;
 	}
-
 	/**
-	 * Create the buckets after warmup time.
-	 * Can be overriden to customize buckets configuration.
-	 * By default buckets are create with:<ul>
-	 * <li>Min: stopwatch min-10% rounded to inferior millisecond</li>
-	 * <li>Max: stopwatch max+10 rounded to superior millisecond</li>
-	 * <li>Nb buckets: {@link #bucketNb}
-	 *
-	 * @param stopwatch Stopwatch (containing configuration)
-	 * @return new Buckets objects
+	 * Get the buckets attribute.
 	 */
-	protected Buckets createBuckets(Stopwatch stopwatch) {
-		if (stopwatch.getCounter() > warmupCounter) {
-			// Compute min
-			long min = stopwatch.getMin() * 90L / 100L; // min -10%
-			min = Math.max(0, min); // no negative mins
-			min = (min / SimonUtils.NANOS_IN_MILLIS) * SimonUtils.NANOS_IN_MILLIS; // round to lower millisecond
-			// Compute max
-			long max = (stopwatch.getMax() * 110L) / 100L; // max +10%
-			max = (max / SimonUtils.NANOS_IN_MILLIS + 1) * SimonUtils.NANOS_IN_MILLIS; // round to upper millisecond
-			Buckets buckets = new Buckets(min, max, bucketNb);
-			buckets.setLogTemplate(createLogTemplate(stopwatch));
-			return buckets;
-		} else {
-			return null;
-		}
+	public static Buckets getBuckets(Stopwatch stopwatch) {
+		return (Buckets) stopwatch.getAttribute(ATTR_NAME_BUCKETS);
 	}
-
+	/**
+	 * Factory method to create a Buckets object using given configuration
+	 * @param stopwatch Target Stopwatch
+	 * @param min Min bound
+	 * @param max Max bound
+	 * @param bucketNb Number of buckets between min and max
+	 * @return Buckets
+	 */
+	protected final Buckets createBuckets(Stopwatch stopwatch, long min, long max, int bucketNb) {
+		Buckets buckets = new Buckets(min, max, bucketNb);
+		buckets.setLogTemplate(createLogTemplate(stopwatch));
+		return buckets;
+	}
+	
+	/**
+	 * Create Buckets for given stopwatch.
+	 * Call {@link #createBuckets(org.javasimon.Stopwatch, long, long, int) to create a new buckets object.
+	 * @param stopwatch Stopwatch
+	 * @return Buckets
+	 */
+	protected  abstract Buckets createBuckets(Stopwatch stopwatch);
+	
 	/**
 	 * Get the buckets attribute or create it if it does not exist.
 	 */
 	@SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
-	private Buckets getOrCreateBuckets(Stopwatch stopwatch) {
+	protected final Buckets getOrCreateBuckets(Stopwatch stopwatch) {
 		synchronized (stopwatch) {
 			Buckets buckets = getBuckets(stopwatch);
 			if (buckets == null) {
 				buckets = createBuckets(stopwatch);
-				if (buckets != null) {
-					// Transform buckets values into buckets
-					buckets.addValues(getBucketsValues(stopwatch));
-					removeBucketsValues(stopwatch);
-					stopwatch.setAttribute(ATTR_NAME_BUCKETS, buckets);
-				}
+				stopwatch.setAttribute(ATTR_NAME_BUCKETS, buckets);
 			}
 			return buckets;
 		}
-	}
-
-	/**
-	 * Get the buckets attribute.
-	 */
-	private static Buckets getBuckets(Stopwatch stopwatch) {
-		return (Buckets) stopwatch.getAttribute(ATTR_NAME_BUCKETS);
 	}
 
 	/**
@@ -213,66 +125,54 @@ public class QuantilesCallback extends CallbackSkeleton {
 	}
 
 	/**
-	 * When simon is created, the list containing Split values is added to stopwatch attributes.
-	 */
-	@Override
-	public void onSimonCreated(Simon simon) {
-		if (simon instanceof Stopwatch) {
-			Stopwatch stopwatch = (Stopwatch) simon;
-			getOrCreateBucketsValues(stopwatch);
-		}
-	}
-	/**
-	 * Method called when there is a new split on a Stopwatch, either
+	 * Called when there is a new split on a Stopwatch, either
 	 * {@link #onStopwatchStop} or {@link #onStopwatchAdd}.
-	 * When a new Split is ready, if buckets has been initialized value
-	 * is added to appropriate bucket. Else if stopwatch is warming up
-	 * value is added to value list.
+	 * If buckets have been initialized, the value is added to appropriate bucket.
 	 */
-	private void onStopwatchSplit(Split split) {
-		final Stopwatch stopwatch = split.getStopwatch();
+	protected void onStopwatchSplit(Stopwatch stopwatch, Split split) {
 		Buckets buckets = getOrCreateBuckets(stopwatch);
-		long value = split.runningFor();
-		if (buckets == null) {
-			// Warming up
-			getOrCreateBucketsValues(stopwatch).add(value);
-		} else {
-			// Warm
-			buckets.addValue(value);
+		if (buckets != null) {
+			buckets.addValue(split.runningFor());
 			buckets.log(split);
 		}
 	}
+	/**
+	 * Called internally when there is a new split on a Stopwatch
+	 * from {@link #onStopwatchAdd(org.javasimon.Stopwatch, long, org.javasimon.StopwatchSample).
+	 * If buckets have been initialized, the value is added to appropriate bucket.
+	 */
+	protected void onStopwatchSplit(Stopwatch stopwatch, long value) {
+		Buckets buckets = getOrCreateBuckets(stopwatch);
+		if (buckets != null) {
+			buckets.addValue(value);
+		}
+	}
 
 	/**
-	 * When a Split is stopped, if buckets has been initialized value
-	 * is added to appropriate bucket. Else if stopwatch is warming up
-	 * value is added to value list.
+	 * When a split is stopped, if buckets have been initialized, the value
+	 * is added to appropriate bucket.
 	 */
 	@Override
 	public void onStopwatchStop(Split split, StopwatchSample sample) {
-		onStopwatchSplit(split);
+		onStopwatchSplit(split.getStopwatch(), split);
 	}
 
 	/**
-	 * When a Split is a, if buckets has been initialized value
-	 * is added to appropriate bucket. Else if stopwatch is warming up
-	 * value is added to value list.
+	 * When a split is added, if buckets have been initialized, the value
+	 * is added to appropriate bucket.
 	 */
 	@Override
 	public void onStopwatchAdd(Stopwatch stopwatch, Split split, StopwatchSample sample) {
-		onStopwatchSplit(split);
+		onStopwatchSplit(split.getStopwatch(), split);
 	}
-
+	
+	/**
+	 * When a split is added, if buckets have been initialized, the value
+	 * is added to appropriate bucket.
+	 */
 	@Override
 	public void onStopwatchAdd(Stopwatch stopwatch, long value, StopwatchSample sample) {
-		Buckets buckets = getOrCreateBuckets(stopwatch);
-		if (buckets == null) {
-			// Warming up
-			getOrCreateBucketsValues(stopwatch).add(value);
-		} else {
-			// Warm
-			buckets.addValue(value);
-		}
+		onStopwatchSplit(stopwatch, value);
 	}
 
 	/**
@@ -283,17 +183,10 @@ public class QuantilesCallback extends CallbackSkeleton {
 		if (simon instanceof Stopwatch) {
 			Stopwatch stopwatch = (Stopwatch) simon;
 			Buckets buckets = getBuckets(stopwatch);
-			if (buckets == null) {
-				// Cold
-				List<Long> values = getBucketsValues(stopwatch);
-				if (values != null) {
-					// Warming up
-					values.clear();
-				}
-			} else {
-				// Warm
+			if (buckets != null) {
 				buckets.clear();
 			}
 		}
 	}
+
 }
