7d8f6bbdfeaed9b1e2a2066d31382bfdce5cc63d
gerald.quintana@gmail.com <gerald.quintana@gmail.com@b27f8733-aa53-0410-bf41-0b3b0dae0d5f>
2012-10-14 13:38:06 +0000

    Refactored JettyMain to make it more readable
    git-svn-id: https://javasimon.googlecode.com/svn/trunk@654 b27f8733-aa53-0410-bf41-0b3b0dae0d5f

--- console/embed/src/test/java/org/javasimon/console/JettyMain.java
+++ console/embed/src/test/java/org/javasimon/console/JettyMain.java
@@ -1,10 +1,5 @@
 package org.javasimon.console;
 
-import java.util.Random;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -16,136 +11,200 @@ import org.javasimon.callback.CompositeCallbackImpl;
 import org.javasimon.callback.async.AsyncCallbackProxyFactory;
 import org.javasimon.callback.calltree.CallTreeCallback;
 import org.javasimon.callback.quantiles.AutoQuantilesCallback;
-import org.javasimon.callback.quantiles.FixedQuantilesCallback;
-import org.javasimon.callback.quantiles.QuantilesCallback;
 import org.javasimon.callback.timeline.TimelineCallback;
 import org.javasimon.console.plugin.CallTreeDetailPlugin;
-import org.javasimon.console.plugin.DummyDetailPlugin;
 import org.javasimon.console.plugin.QuantilesDetailPlugin;
 import org.javasimon.console.plugin.TimelineDetailPlugin;
 import org.javasimon.utils.SimonUtils;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Main using Jetty to test Simon Console.
  * @author gquintana
  */
 public class JettyMain {
-	private static Lock lock=new ReentrantLock();
-	public static void main(String[] args) {
-		try {
-			// Server
-			Server server = new Server(8080);
-			// Context
-			ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-			context.setContextPath("/");
-			server.setHandler(context);
-			// Servlet
-			CompositeCallback compositeCallback=new CompositeCallbackImpl();
-			compositeCallback.addCallback(new AutoQuantilesCallback(5, 5));
+    /**
+     * Jetty Server
+     */
+    private Server server;
+	private Lock lock=new ReentrantLock();
+    /**
+     * Time for changing Simons
+     */
+    private Timer timer=new Timer();
+    /**
+     * Random generator to generate Simons
+     */
+    private final RandomHelper random=new RandomHelper();
+    /**
+     * Initialize Jetty Server
+     */
+	private void initServer() {
+        // Server
+        server = new Server(8080);
+        // Context
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        // Callbacks
+        CompositeCallback compositeCallback=new CompositeCallbackImpl();
+        compositeCallback.addCallback(new AutoQuantilesCallback(5, 5));
 //			compositeCallback.addCallback(new FixedQuantilesCallback(0L, 200L, 5));
-			compositeCallback.addCallback(new CallTreeCallback(50));
-			compositeCallback.addCallback(new TimelineCallback(10, 60000L));
-			SimonManager.callback().addCallback(new AsyncCallbackProxyFactory(compositeCallback).newProxy());
-			SimonData.initialize();
-			Timer timer=new Timer();
-			timer.schedule(new TimerTask(){
-				final Stopwatch tlStopwatch=SimonManager.getStopwatch("TL");
-				@Override
-				public void run() {
-					try {
-						lock.lock();
-						System.out.println("TL "+addRandomStopwatchSplit(tlStopwatch));
-					} finally {
-						lock.unlock();
-					}
-				}
-			}, 0, 10000L);
-			addSimons("Z",4);
-			addStackedSimons();
-			final SimonConsoleServlet simonConsoleServlet = new SimonConsoleServlet();
-			ServletHolder servletHolder=new ServletHolder(simonConsoleServlet);
-			servletHolder.setInitParameter("console-path", "");
-			servletHolder.setInitParameter("plugin-classes", 
-					QuantilesDetailPlugin.class.getName()
-					+","+CallTreeDetailPlugin.class.getName()
-					+","+TimelineDetailPlugin.class.getName());
-			context.addServlet(servletHolder, "/*");
-			// Start server thread
-			server.start();
-			simonConsoleServlet.getRequestProcessor().getPluginManager().addPlugin(new DummyDetailPlugin());
-			server.join();
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		}
-	}
-	private static final Random random=new Random();
-	private static void addSimons(String prefix,int depth)  {
+        compositeCallback.addCallback(new CallTreeCallback(50));
+        compositeCallback.addCallback(new TimelineCallback(10, 60000L));
+        SimonManager.callback().addCallback(new AsyncCallbackProxyFactory(compositeCallback).newProxy());
+        // Simon Servlet
+        final SimonConsoleServlet simonConsoleServlet = new SimonConsoleServlet();
+        ServletHolder servletHolder=new ServletHolder(simonConsoleServlet);
+        servletHolder.setInitParameter("console-path", "");
+        servletHolder.setInitParameter("plugin-classes",
+                QuantilesDetailPlugin.class.getName()
+                +","+CallTreeDetailPlugin.class.getName()
+                +","+TimelineDetailPlugin.class.getName());
+        context.addServlet(servletHolder, "/*");
+    }
+
+    /**
+     * Add Simons
+     */
+    private void initData() {
+        addDefaultSimons();
+        addChangingSimons();
+        addStackedSimons();
+        addManySimons();
+    }
+
+    /**
+     * Run Jetty and wait till it stops
+     */
+    private void runAndWait() throws Exception {
+        // Start server thread
+        server.start();
+        server.join();
+    }
+
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        try {
+            JettyMain main=new JettyMain();
+            main.initServer();
+            main.initData();
+            main.runAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Add basic simons A, B, C and X.
+     * X is used to test Counter rendering
+     * 
+     */
+    private void addDefaultSimons() {
+        SimonData.initialize();
+    }
+
+    /**
+     * Starts a timer which changes Simons values.
+     * TL is used to test Timeline and Quantiles plugins rendering
+     */
+    private void addChangingSimons() {
+        timer.schedule(new TimerTask(){
+            final Stopwatch tlStopwatch= SimonManager.getStopwatch("TL");
+            @Override
+            public void run() {
+                try {
+                    lock.lock();
+                    System.out.println("TL "+ addStopwatchSplit(tlStopwatch));
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }, 0, 10000L);
+    }
+
+    /**
+     * Add many Simons for performances testing
+     * Z.* Simons are used for performance testing
+     */
+    private void addManySimons() {
+        new Thread() {
+            @Override
+            public void run() {
+                addManySimons("Z", 4, 3, 3, 6);
+            }
+        }.start();
+
+    }
+    private static final String ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    /**
+     * Recursively Add many Simons for performances testing
+     */
+	private void addManySimons(String prefix, int depth, int groupWidth, int leafWidth, int splitWidth)  {
 		if (depth==0) {
-			addStopwatches(prefix);
+            // Generate Simons of type Stopwatch
+            final int sibblings = random.randomInt(Math.min(1, groupWidth / 2), leafWidth);
+            for(int i=0;i<sibblings;i++) {
+                String name=prefix+"."+ALPHABET.charAt(i);
+                addStopwatchSplits(SimonManager.getStopwatch(name), splitWidth);
+            }
 		} else {
-			addGroups(prefix,depth);
+            // Generate Simons of type Unknown
+            final int sibblings = random.randomInt(Math.min(1, groupWidth / 2), groupWidth);
+            for(int i=0;i<sibblings;i++) {
+                String name=prefix+"."+ALPHABET.charAt(i);
+                addManySimons(name, depth - 1, groupWidth, leafWidth, splitWidth);
+            }
 		}
 	}
-	private static int randomInt(int min, int max) {
-		return min+random.nextInt(max-min);
-	}
-	/**
-	 * Wait random time 
-	 * @return Waited random time
-	 */
-	private static long randomWait() {
-		final long waitTime=randomInt(50, 100);
-		try {
-			Thread.sleep(waitTime);
-		} catch (InterruptedException interruptedException) {
-		}
-		return waitTime;
-	}
-	private static long addRandomStopwatchSplit(Stopwatch stopwatch) {
+
+    /**
+     * Generate a split for a Stopwatch
+     */
+	private long addStopwatchSplit(Stopwatch stopwatch) {
 		Split split=stopwatch.start();
 		try {
-			randomWait();
+			random.randomWait(50,150);
 		} finally {
 			split.stop();
 		}
 		return split.runningFor()/SimonUtils.NANOS_IN_MILLIS;
 	}
-	private static void addRandomStopwatchSplits(final Stopwatch stopwatch, final int splits) {
-		for(int i=0;i<splits;i++) {
-			System.out.print(addRandomStopwatchSplit(stopwatch)+",");
-		}
-	}
-	private static void addStopwatches(String name) {
-		final int splits = randomInt(4,8);
-		final Stopwatch stopwatch = SimonManager.getStopwatch(name);
+
+    /**
+     * Generate a Simon of type "Stopwatch" and fill it with some Splits
+     * @param splitWidth Max number of splits per Stopwatch
+     */
+	private void addStopwatchSplits(Stopwatch stopwatch, int splitWidth) {
+		final int splits = random.randomInt(splitWidth / 2, splitWidth);
 		try {
 			lock.lock();
-			System.out.print(name+" "+splits+": ");
-			addRandomStopwatchSplits(stopwatch, splits);
+			System.out.print(stopwatch.getName()+" "+splits+": ");
+            for(int i=0;i<splits;i++) {
+          		System.out.print(addStopwatchSplit(stopwatch)+",");
+          	}
 			System.out.println();
 		} finally {
 			lock.unlock();
 		}
 	}
-	private static void addGroups(String namePrefix, int depth) {
-		final int sibblings = randomInt(1,3);
-		for(int i=0;i<sibblings;i++) {
-			char c=(char)('A'+i);
-			String name=namePrefix+"."+new String(new char[]{c});
-			addSimons(name, depth-1);
-		}
-	}
 	/**
 	 * Stacked stopwatches to test call tree
 	 */
-	private static void addStackedSimons() {
+	private void addStackedSimons() {
 		Split splitA=SimonManager.getStopwatch("Y.A").start();
 		Split splitB=SimonManager.getStopwatch("Y.B").start();
-		addRandomStopwatchSplits(SimonManager.getStopwatch("Y.C"), 3);
+		addStopwatchSplits(SimonManager.getStopwatch("Y.C"), 6);
 		splitB.stop();
 		Split splitD=SimonManager.getStopwatch("Y.D").start();
-		randomWait();
-		randomWait();
+		random.randomWait(100,250);
+		random.randomWait(100,250);
 		splitD.stop();
 		splitA.stop();
 	}
