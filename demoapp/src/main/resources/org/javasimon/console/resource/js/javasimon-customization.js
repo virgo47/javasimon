1dc091b0c09563cafc68bbe40b9d8f74a40ae7cf
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2013-11-17 18:06:33 +0200

    Implemented customization for Tree view. Added guard against not defined customization callbacks.

--- demoapp/src/main/resources/org/javasimon/console/resource/js/javasimon-customization.js
+++ demoapp/src/main/resources/org/javasimon/console/resource/js/javasimon-customization.js
@@ -6,11 +6,24 @@ $.getScript("resource/js/javasimon-util.js", function(data, textStatus, jqxhr) {
     console.log("javasimon-utils were loaded");
 });
 
-javasimon.onJsonData = function(json) {
+javasimon.onTableData = function(json) {
     $.each(json, function(index, sample) {
         var $sampleRow = javasimon.DOMUtil.fnGetSampleRow(sample.name);
         if (sample.max > 20) {
             $sampleRow.css('background-color','#ff0000');
         }
     });
+}
+
+function isLeaf(treeElement) {
+    return treeElement.bHasChildren === false;
+}
+
+javasimon.onTreeElementDrawn = function(treeElement) {
+    if (isLeaf(treeElement)) {
+        var $sampleRow = javasimon.DOMUtil.fnGetSampleRow(treeElement.oData.name);
+        if (treeElement.oData.max > 20) {
+            $sampleRow.css('background-color','#ff0000');
+        }
+    }
 }
