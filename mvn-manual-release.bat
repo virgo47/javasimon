185d37b4f8df6e6364ef5fe247001ac8db675663
Richard Virgo Richter <virgo47@gmail.com>
2014-10-12 22:40:55 +0200

    jdbc41 must be treated manually

--- mvn-manual-release.bat
+++ mvn-manual-release.bat
@@ -6,8 +6,13 @@ call mvn versions:set -DnewVersion=%VERSION%
 call mvn version:commit
 set JAVA_HOME=%JAVA6_HOME%
 call mvn clean deploy -P sonatype-oss-release -Dgpg.passphrase=%GPGPP%
-cd jdbc41
-call mvn versions:set -DnewVersion=%VERSION%
-call mvn version:commit
+
+REM would be great if "update-parent" worked for older versions, but it does not
+REM that's why for jdbc41 parent version must be set manualy BEFORE running this script
+pushd jdbc41
+REM call mvn versions:update-parent -DparentVersion=%VERSION%
+REM call mvn version:commit
 set JAVA_HOME=%JAVA7_HOME%
 call mvn clean deploy -P sonatype-oss-release -Dgpg.passphrase=%GPGPP%
+
+popd
