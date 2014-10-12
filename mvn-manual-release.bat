5ee8b15b7c63ae33633819a17c9556b174d3db3f
Richard Virgo Richter <virgo47@gmail.com>
2014-10-12 22:40:55 +0200

    3.5.2 how it was released + release batch file that should have existed long time ago ;-)

---
+++ mvn-manual-release.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+set /P VERSION="Enter new version: "
+set /P GPGPP="Enter GPG pass-phrase: "
+
+call mvn versions:set -DnewVersion=%VERSION%
+call mvn version:commit
+set JAVA_HOME=%JAVA6_HOME%
+call mvn clean deploy -P sonatype-oss-release -Dgpg.passphrase=%GPGPP%
+cd jdbc41
+call mvn versions:set -DnewVersion=%VERSION%
+call mvn version:commit
+set JAVA_HOME=%JAVA7_HOME%
+call mvn clean deploy -P sonatype-oss-release -Dgpg.passphrase=%GPGPP%
