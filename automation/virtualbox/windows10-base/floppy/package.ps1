1510ba470ae854ee2a08f2b4b284a26fdc4d0b43
Richard Virgo Richter <virgo47@gmail.com>
2016-01-27 23:18:05 +0100

    activated removal of features + many metro applications + icons from taskbar (pinned apps reappear though) + Install-WindowsUpdate

--- automation/virtualbox/windows10-base/floppy/package.ps1
+++ automation/virtualbox/windows10-base/floppy/package.ps1
@@ -4,12 +4,39 @@ netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 
 
 Update-ExecutionPolicy -Policy Unrestricted
 
-Write-BoxstarterMessage "Removing unused features...DISABLED"
+Write-BoxstarterMessage "Removing unused features..."
 Import-Module dism
-#Get-WindowsOptionalFeature -online | where {$_.State -eq 'disabled'} | Disable-WindowsOptionalFeature -Remove -online
+Get-WindowsOptionalFeature -online | where {$_.State -eq 'disabled'} | Disable-WindowsOptionalFeature -Remove -online
 
-Write-BoxstarterMessage "Installing Windows Updtates...DISABLED"
-#Install-WindowsUpdate -AcceptEula
+Write-BoxstarterMessage "Installing Windows Updtates..."
+Install-WindowsUpdate -AcceptEula
+
+# Remove modern apps
+Get-AppxPackage -Name *WindowsCamera* | Remove-AppxPackage
+Get-AppxPackage -Name *ZuneMusic* | Remove-AppxPackage
+Get-AppxPackage -Name *WindowsMaps* | Remove-AppxPackage
+Get-AppxPackage -Name *MicrosoftSolitaireCollection* | Remove-AppxPackage
+Get-AppxPackage -Name *BingFinance* | Remove-AppxPackage
+Get-AppxPackage -Name *ZuneVideo* | Remove-AppxPackage
+Get-AppxPackage -Name *BingNews* | Remove-AppxPackage
+Get-AppxPackage -Name *WindowsPhone* | Remove-AppxPackage
+Get-AppxPackage -Name *Windows.Photos* | Remove-AppxPackage
+Get-AppxPackage -Name *BingSports* | Remove-AppxPackage
+Get-AppxPackage -Name *XboxApp* | Remove-AppxPackage
+Get-AppxPackage -Name *BingWeather* | Remove-AppxPackage
+Get-AppxPackage -Name *WindowsSoundRecorder* | Remove-AppxPackage
+Get-AppxPackage -Name *3DBuilder* | Remove-AppxPackage
+Get-AppxPackage -Name *SkypeApp* | Remove-AppxPackage
+Get-AppxPackage -Name *MicrosoftOfficeHub* | Remove-AppxPackage
+
+# removing Show Task View icon
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'ShowTaskViewButton' -Value 0
+# disabling Cortana
+New-Item 'HKCU:\Software\Policies\Microsoft\Windows\Windows Search' -Force | New-ItemProperty -Name 'AllowCortana' -PropertyType 'DWord' -Value 0 -Force | Out-Null
+New-Item 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -Force | New-ItemProperty -Name  'SearchboxTaskbarMode' -PropertyType 'DWord' -Value 0 -Force | Out-Null
+# TODO: cleaning pinned-to-tasklist applications - this normally works, but does not have effect after vagrant up
+Remove-Item "$Env:AppData\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*"
+Remove-Item "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband"
 
 Write-BoxstarterMessage "Removing page file"
 $pageFileMemoryKey = "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management"
