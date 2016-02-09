0f7c9d27b33d58262ce8e838d6052737474d2b1b
Richard "Virgo" Richter <virgo47@gmail.com>
2016-02-09 15:42:05 +0100

    finished schema exporter for tables, pk/fk/indexes(uniq/not) and checks (SQL Server specific)

--- minis/copypastes/src/main/groovy/dbutils/SchemaExporter.groovy
+++ minis/copypastes/src/main/groovy/dbutils/SchemaExporter.groovy
@@ -2,17 +2,26 @@ package dbutils
 
 import groovy.transform.Field
 
+import java.sql.DatabaseMetaData
 import java.sql.ResultSet
 
 import static dbutils.DbInit.connect
-import static dbutils.DbInit.getSql
+import static dbutils.DbInit.sql
 
-// names we want to escape
-@Field List specialNames = ['order', 'action', 'default', 'group', 'user', 'sign', 'from', 'to', 'key']
+class SpecialColumns {
+	static List specialNames = ['action', 'default', 'from', 'group', 'key', 'order', 'sign', 'to', 'user']
+	static String specialNamesRegex = specialNames.join('|')
+
+	static String escape(String columnName) {
+		columnName.replaceAll("^($specialNamesRegex)\$", '"$1"')
+	}
+
+	static List<String> escapeAll(List<String> columnNames) {
+		columnNames.collect { escape(it) }
+	}
+}
 
 @Field def sqlOutput = System.out // stream or writer, default is stdout
-
-@Field String dir = "copypastes/src/main/resources/h2/"
 
 @Field Map<String, TableDef> tableDefs = emptyTableDefs(
 	'GuiUsers', 'BankTransactions')
@@ -25,14 +34,18 @@ def catalog = System.getProperty("jdbc.catalog", "catalog")
 
 connect(url, user, password)
 
+def schemaPattern = 'dbo' // SQL Server
+
 def metaData = sql.connection.metaData
 
-investigateColumns(metaData.getColumns(catalog, null, null, null))
+investigateColumns(metaData.getColumns(catalog, schemaPattern, null, null))
 tableDefs.keySet().each {
-	investigatePrimaryKeys(metaData.getPrimaryKeys(catalog, null, it))
-	investigateConstraints(metaData.getImportedKeys(catalog, null, it))
+	investigatePrimaryKeys(metaData.getPrimaryKeys(catalog, schemaPattern, it))
+	investigateForeignKeys(metaData.getImportedKeys(catalog, schemaPattern, it))
+	investigateIndexes(metaData.getIndexInfo(catalog, schemaPattern, it, false, false))
 }
-printSchema(tableDefs)
+investigateCheckConstraints(metaData)
+printSchema('schema.sql')
 
 private void investigateColumns(ResultSet rs) {
 	processResultSet(rs, 'TABLE_NAME') { tableDef ->
@@ -43,6 +56,7 @@ private void investigateColumns(ResultSet rs) {
 		columnDef.decimalDigits = rs.getInt('DECIMAL_DIGITS')
 		columnDef.nullable = yesNo(rs, 'IS_NULLABLE')
 		columnDef.autoIncrement = yesNo(rs, 'IS_AUTOINCREMENT')
+		columnDef.defaultValue = stripOuterParenthesis(rs.getString('COLUMN_DEF'))
 
 		tableDef.columns.add(columnDef)
 	}
@@ -59,40 +73,52 @@ private void investigatePrimaryKeys(ResultSet rs) {
 	}
 }
 
-def investigateConstraints(ResultSet rs) {
+def investigateForeignKeys(ResultSet rs) {
 	processResultSet(rs, 'FKTABLE_NAME') { TableDef tableDef ->
 		String fkName = rs.getString('FK_NAME')
-		def foreignKey = tableDef.foreignKeys.computeIfAbsent(fkName, { new ForeignKey(fkName)})
+		def foreignKey = tableDef.foreignKeys.computeIfAbsent(fkName, { new ForeignKey(fkName) })
 		foreignKey.columnNames.add(rs.getString('FKCOLUMN_NAME'))
 		foreignKey.refColumnNames.add(rs.getString('PKCOLUMN_NAME'))
 		foreignKey.refTableName = rs.getString('PKTABLE_NAME')
 	}
 }
 
+def investigateIndexes(ResultSet rs) {
+	processResultSet(rs, 'TABLE_NAME') { TableDef tableDef ->
+		def columnName = rs.getString('COLUMN_NAME')
+		String indexName = rs.getString('INDEX_NAME')
+		if (columnName != null && !indexName.startsWith('PK_')) {
+			def index = tableDef.indexes.computeIfAbsent(indexName, {
+				new Index(indexName, tableDef.tableName)
+			})
+			index.columnNames.add(columnName)
+			index.unique = !rs.getBoolean('NON_UNIQUE')
+			// TODO: do we want ASC/DESC? For H2 it probably doesn't matter that much
+		}
+	}
+}
+
 private void processResultSet(ResultSet rs, String tableColName, Closure processColumn) {
 	while (rs.next()) {
-		def tableDef = getTableDef(rs.getString(tableColName))
+		def tableName = rs.getString(tableColName)
+		def tableDef = getTableDef(tableName)
 		if (tableDef == null) {
 			continue
 		};
+		// we fill in the table name "lazily" from metadata, so the casing is from DB, not from our list
+		if (tableDef.tableName == null) {
+			tableDef.tableName = tableName
+		}
 
 		processColumn tableDef
 	}
 	rs.close()
 }
 
-private Map<String, TableDef> printSchema(Map<String, TableDef> tableDefs) {
+def printSchema(String outFileName) {
+	sqlOutput = new FileWriter(outFileName)
 	tableDefs.each { k, v ->
-		sqlOutput.println v
-	}
-}
-
-// OLD from SqlExportAsInsert
-def dumpTables(String outFileName, String... tableSpecs) {
-	println "\nPreparing $outFileName"
-	sqlOutput = new FileWriter(dir + outFileName)
-	tableSpecs.each {
-		dumpTable(it)
+		sqlOutput.println "\n-----------------------------------------------------------\n$v"
 	}
 	sqlOutput.flush()
 }
@@ -102,23 +128,28 @@ boolean yesNo(ResultSet resultSet, String columnName) {
 }
 
 class TableDef {
-	final String tableName
+	String tableName
 	List<ColumnDef> columns = new ArrayList<>()
 	PrimaryKey primaryKey;
 	Map<String, ForeignKey> foreignKeys = new LinkedHashMap<>()
-
-	TableDef(String name) {
-		tableName = name
-	}
+	Map<String, Index> indexes = new LinkedHashMap<>()
+	List otherConstraints = new ArrayList<>()
 
 	@Override
+	@SuppressWarnings("GroovyAssignabilityCheck")
 	String toString() {
-		return """CREATE TABLE $tableName (
-	${columns.join(",\n\t")},
+		def afterColumnList = new ArrayList()
+		if (primaryKey != null) {
+			afterColumnList.add(primaryKey)
+		}
+		afterColumnList.addAll(foreignKeys.values())
+		def alterList = indexes.values() + otherConstraints
 
-	${primaryKey}${foreignKeys.isEmpty() ? '' : ',\n\t' + foreignKeys.values().join(",\n\t")}
-);
-"""
+		return """CREATE TABLE $tableName (
+	${columns.join(',\n\t')}${
+			afterColumnList.isEmpty() ? '' : ',\n\n\t' + afterColumnList.join(',\n\t')
+		}
+)${alterList.isEmpty() ? '' : ';\n\n' + alterList.join(';\n')};"""
 	}
 }
 
@@ -128,50 +159,50 @@ class ColumnDef {
 	int size
 	int decimalDigits
 	boolean nullable
-
 	boolean autoIncrement
+	String defaultValue
 
 	String sqlTypeString() {
 		switch (sqlType) {
-			case -7: return "BIT"; // BIT
-			case -6: return "TINYINT"; // TINYINT
-			case 5: return "SMALLINT"; // SMALLINT
-			case 4: return "INT"; // INTEGER
-			case -5: return "BIGINT"; // BIGINT
-			case 6: return "FLOAT"; // FLOAT
-			case 7: return "REAL"; // REAL
-			case 8: return "DOUBLE"; // DOUBLE
-			case 2: return withSizeAndPrecision("NUMERIC"); // NUMERIC
-			case 3: return withSizeAndPrecision("DECIMAL"); // DECIMAL
-			case 1: return withSize("CHAR"); // CHAR
-			case 12: return withSize("VARCHAR"); // VARCHAR
-			case -1: return withSize("LONGVARCHAR"); // LONGVARCHAR
-			case 91: return "DATE"; // DATE
-			case 92: return "TIME"; // TIME
-			case 93: return "TIMESTAMP"; // TIMESTAMP
-			case -2: return "BINARY"; // BINARY
-			case -3: return "VARBINARY"; // VARBINARY
-			case -4: return "LONGVARBINARY"; // LONGVARBINARY
-			case 0: return "NULL"; // NULL
-			case 1111: return "OTHER"; // OTHER
-			case 2000: return "JAVA_OBJECT"; // JAVA_OBJECT
-			case 2001: return "DISTINCT"; // DISTINCT
-			case 2002: return "STRUCT"; // STRUCT
-			case 2003: return "ARRAY"; // ARRAY
-			case 2004: return "BLOB"; // BLOB
-			case 2005: return "CLOB"; // CLOB
-			case 2006: return "REF"; // REF
-			case 70: return "DATALINK"; // DATALINK
-			case 16: return "BOOLEAN"; // BOOLEAN
-			case -8: return "ROWID"; // ROWID
-			case -15: return withSize("NCHAR"); // NCHAR
-			case -9: return withSize("NVARCHAR"); // NVARCHAR
-			case -16: return withSize("LONGNVARCHAR"); // LONGNVARCHAR
-			case 2011: return "NCLOB"; // NCLOB
-			case 2009: return "SQLXML"; // SQLXML
-			case 2012: return "REF_CURSOR"; // REF_CURSOR
-			case 2013: return "TIME_WITH_TIMEZONE"; // TIME_WITH_TIMEZONE
-			case 2014: return "TIMESTAMP_WITH_TIMEZONE"; // TIMESTAMP_WITH_TIMEZONE
+			case -7: return 'BIT'; // BIT
+			case -6: return 'TINYINT'; // TINYINT
+			case 5: return 'SMALLINT'; // SMALLINT
+			case 4: return 'INT'; // INTEGER
+			case -5: return 'BIGINT'; // BIGINT
+			case 6: return 'FLOAT'; // FLOAT
+			case 7: return 'REAL'; // REAL
+			case 8: return 'DOUBLE'; // DOUBLE
+			case 2: return withSizeAndPrecision('NUMERIC'); // NUMERIC
+			case 3: return withSizeAndPrecision('DECIMAL'); // DECIMAL
+			case 1: return withSize('CHAR'); // CHAR
+			case 12: return withSize('VARCHAR'); // VARCHAR
+			case -1: return withSize('LONGVARCHAR'); // LONGVARCHAR
+			case 91: return 'DATE'; // DATE
+			case 92: return 'TIME'; // TIME
+			case 93: return 'TIMESTAMP'; // TIMESTAMP
+			case -2: return implyDefault('UUID', 'RANDOM_UUID()'); // BINARY
+			case -3: return 'VARBINARY'; // VARBINARY
+			case -4: return 'LONGVARBINARY'; // LONGVARBINARY
+			case 0: return 'NULL'; // NULL
+			case 1111: return 'OTHER'; // OTHER
+			case 2000: return 'JAVA_OBJECT'; // JAVA_OBJECT
+			case 2001: return 'DISTINCT'; // DISTINCT
+			case 2002: return 'STRUCT'; // STRUCT
+			case 2003: return 'ARRAY'; // ARRAY
+			case 2004: return 'BLOB'; // BLOB
+			case 2005: return 'CLOB'; // CLOB
+			case 2006: return 'REF'; // REF
+			case 70: return 'DATALINK'; // DATALINK
+			case 16: return 'BOOLEAN'; // BOOLEAN
+			case -8: return 'ROWID'; // ROWID
+			case -15: return withSize('NCHAR'); // NCHAR
+			case -9: return withSize('NVARCHAR'); // NVARCHAR
+			case -16: return withSize('LONGNVARCHAR'); // LONGNVARCHAR
+			case 2011: return 'NCLOB'; // NCLOB
+			case 2009: return 'SQLXML'; // SQLXML
+			case 2012: return 'REF_CURSOR'; // REF_CURSOR
+			case 2013: return 'TIME_WITH_TIMEZONE'; // TIME_WITH_TIMEZONE
+			case 2014: return 'TIMESTAMP_WITH_TIMEZONE'; // TIMESTAMP_WITH_TIMEZONE
 			default: throw new RuntimeException("Unknown sqlType $sqlType")
 		}
 	}
@@ -184,17 +215,38 @@ class ColumnDef {
 		"$type($size, $decimalDigits)"
 	}
 
+	String implyDefault(String type, String defaultValue) {
+		this.defaultValue = defaultValue
+		return type
+	}
+
+	String defaultValueString() {
+		defaultValue
+			.replace('getdate()', currentDateFunction())
+			.replace('newid()', 'RANDOM_UUID()')
+	}
+
+	String currentDateFunction() {
+		sqlType == 91 ? 'CURRENT_DATE()'
+			: sqlType == 93 || sqlType == 2014 ? 'CURRENT_TIMESTAMP()'
+			: 'CURRENT_TIME()'
+	}
+
 	@Override
 	String toString() {
-		StringBuilder sb = new StringBuilder(name).append(' ').append(sqlTypeString())
+		StringBuilder sb = new StringBuilder(SpecialColumns.escape(name)).append(' ').append(sqlTypeString())
 		if (autoIncrement) {
-			sb.append(" IDENTITY")
+			sb.append(' AUTO_INCREMENT') // IDENTITY implies PK, which sometimes may not be the same
 		}
 		if (!nullable) {
-			sb.append(" NOT NULL")
+			sb.append(' NOT NULL')
+		}
+		if (defaultValue != null) {
+			sb.append(" DEFAULT ").append(defaultValueString())
 		}
 		return sb
 	}
+
 }
 
 class PrimaryKey {
@@ -207,7 +259,7 @@ class PrimaryKey {
 
 	@Override
 	String toString() {
-		return "CONSTRAINT $constraintName PRIMARY KEY (${columnNames.join(", ")})"
+		return "CONSTRAINT $constraintName PRIMARY KEY (${SpecialColumns.escapeAll(columnNames).join(', ')})"
 	}
 }
 
@@ -223,18 +275,84 @@ class ForeignKey {
 
 	@Override
 	String toString() {
-		return "CONSTRAINT $constraintName FOREIGN KEY (${columnNames.join(", ")}) REFERENCES $refTableName (${refColumnNames.join(", ")})"
+		return "CONSTRAINT $constraintName FOREIGN KEY" +
+			" (${SpecialColumns.escapeAll(columnNames).join(', ')})" + " REFERENCES" +
+			" $refTableName (${SpecialColumns.escapeAll(refColumnNames).join(', ')})"
+	}
+}
+
+class Index {
+	final String indexName
+	final String tableName
+	List<String> columnNames = new ArrayList<>()
+	boolean unique;
+
+	Index(String indexName, String tableName) {
+		this.indexName = indexName
+		this.tableName = tableName
+	}
+
+	@Override
+	String toString() {
+		return "CREATE ${unique ? 'UNIQUE ' : ''}INDEX $indexName ON $tableName" +
+			" (${SpecialColumns.escapeAll(columnNames).join(', ')})"
+	}
+}
+
+class CheckConstraint {
+	final String constraintName
+	final String tableName
+	String checkDefinition
+
+	CheckConstraint(String constraintName, String tableName) {
+		this.constraintName = constraintName
+		this.tableName = tableName
+	}
+
+	@Override
+	String toString() {
+		return "ALTER TABLE $tableName ADD CONSTRAINT $constraintName CHECK $checkDefinition"
 	}
 }
 
 Map<String, TableDef> emptyTableDefs(String... tableNames) {
 	def map = new LinkedHashMap<>()
 	tableNames.each {
-		map.put(it.toLowerCase(), new TableDef(it))
+		map.put(it.toLowerCase(), new TableDef())
 	}
 	map
 }
 
 TableDef getTableDef(String tableName) {
 	tableDefs.get(tableName.toLowerCase())
+}
+
+def investigateCheckConstraints(DatabaseMetaData metaData) {
+	if (metaData.getDatabaseProductName().equals("Microsoft SQL Server")) {
+		investigateSqlServerCheckConstraints()
+	}
+}
+
+@SuppressWarnings(["SqlDialectInspection", "SqlNoDataSourceInspection"])
+private void investigateSqlServerCheckConstraints() {
+	ResultSet rs = sql.connection.createStatement().executeQuery(
+		'select cs.name as CONSTRAINT_NAME, cs.definition as DEFINITION, t.name as TABLE_NAME ' +
+			'from sys.check_constraints cs, sys.tables t ' +
+			'where cs.parent_object_id = t.object_id and cs.type = \'C\';')
+	processResultSet(rs, 'TABLE_NAME', { TableDef tableDef ->
+		def constraintName = rs.getString('CONSTRAINT_NAME')
+		def constraint = new CheckConstraint(constraintName, tableDef.tableName)
+		constraint.checkDefinition = processSqlServerCheckDefinition(rs.getString('DEFINITION'))
+		tableDef.otherConstraints.add(constraint)
+	})
+}
+
+String processSqlServerCheckDefinition(String definition) {
+	stripOuterParenthesis(definition
+		.replaceAll("\\[($SpecialColumns.specialNamesRegex)\\]", '"$1"')
+		.replaceAll('\\[(.*?)\\]', '$1'))
+}
+
+String stripOuterParenthesis(String input) {
+	input?.replaceAll(/\((.*)\)/, '$1')
 }
