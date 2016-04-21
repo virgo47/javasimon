10a67b5ef6ba63369ecb1fda8d5ecb7bb9338ada
Richard "Virgo" Richter <virgo47@gmail.com>
2016-04-21 13:34:21 +0200

    discovery that , (implicit cross join) changed to (INNER/CROSS) JOIN works fine

--- issues/h2npe-with-wild-join/src/test/resources/failing-script.sql
+++ issues/h2npe-with-wild-join/src/test/resources/failing-script.sql
@@ -32,14 +32,16 @@ select * from {oj Transactions t2 LEFT OUTER JOIN WFL_States t0
   ON t0.state_id = t2.state AND t0.object_id = 2
 --     without this subquery it works fine _and its WHERE_
     AND EXISTS (SELECT 1 FROM WFL_States_StateGroups t4
---    , WFL_StateGroups t3 -- this can be omitted or switched with t4
+   , WFL_StateGroups t3 -- this can be omitted or switched with t4
       WHERE t4.object_id = t0.object_id -- necessary for NPE
---    AND t3.id = -257  AND t4.state_id = t0.state_id AND t3.id = t4.state_group_id -- this can be omitted
+   AND t3.id = -257  AND t4.state_id = t0.state_id AND t3.id = t4.state_group_id -- this can be omitted
     )
   }
 , TransactionsProperties t1
+-- if ^^^ rewritten as "(CROSS/INNER) JOIN" instead "," - it works fine, e.g.:
+-- CROSS JOIN TransactionsProperties t1
   WHERE t2.client_id = 56
---  AND t0.state_id IS NOT NULL
+  AND t0.state_id IS NOT NULL
   AND t1.transaction_id = t2.id -- this throws NPE
   AND t1.property_id = 804 -- this throws NPE
---  AND t1.value IS NOT NULL -- this condition works
+  AND t1.value IS NOT NULL -- this condition works
