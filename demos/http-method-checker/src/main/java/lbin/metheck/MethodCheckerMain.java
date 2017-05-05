06590c730d0f514fb20d8aec1ce686ff61acfe61
Richard Virgo Richter <virgo47@gmail.com>
2017-05-05 03:58:17 +0200

    HTTP method checker experiment

--- demos/http-method-checker/src/main/java/lbin/metheck/MethodCheckerMain.java
+++ demos/http-method-checker/src/main/java/lbin/metheck/MethodCheckerMain.java
@@ -5,6 +5,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,13 +16,13 @@ public class MethodCheckerMain {
 		SpringApplication.run(MethodCheckerMain.class, args);
 	}
 
-	@RequestMapping("/all")
+	@RequestMapping(value = "/all")
 	public String all() {
 		return "OK-all";
 	}
 
 	@RequestMapping(value = "/get", method = GET)
-	public String get() {
-		return "OK-get";
+	public String get(@RequestParam(name = "zipcode") String zipcode) {
+		return "OK-get" + zipcode;
 	}
 }
